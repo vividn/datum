07fce7fb0f455480d6966bc212f54d0f8cef8cde
Nate Faber <nathaniel@njfaber.com>
2020-06-25 21:13:10 +0200

    Start creating input structure

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log("Hello");
+db = require('nano')('http://localhost:5984/datum');
+
+let argv = require('yargs')
+    .command('datum', 'quickly insert timestamped data into couchdb')
+    .help('h')
+    .alias('h', 'help')
+
+    // TODO
+    .describe('d', 'specify date of the timestamp, use `+n` or `-n` for a date relative to today')
+    .alias('d','date')
+
+    // TODO
+    .describe('t', "specify time of the timestamp, use `+n` or `-n` for a timestamp n minutes relative to now")
+    .alias('t', 'time')
+
+
+    .argv
+
+console.log(argv);
