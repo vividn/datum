f68c142ae54cfea56d736cf031cd2082d55ec5fc
Nate Faber <nathaniel@njfaber.com>
2020-07-09 12:35:45 +0200

    Start making some test cases

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -5,3 +5,10 @@ describe('blah', () => {
     expect(sum(1, 1)).toEqual(2);
   });
 });
+
+const arg_test_cases = {
+  '--abc 123': {abc: 123},
+  'abc=123': {abc: 123},
+  '-K a b c=3 -k 1 2': {a: 1, b:2, c: 3},
+}
+console.log(arg_test_cases)
