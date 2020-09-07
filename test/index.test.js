66558c26cd9b2a31a2bed17706d106c7f9ee67dc
Nate Faber <nathaniel@njfaber.com>
2020-09-07 19:59:10 +0200

    Lint code

--- test/index.test.js
+++ test/index.test.js
@@ -2,35 +2,33 @@ const main = require("../src/index");
 const nano = require("nano")("http://admin:password@localhost:5983");
 
 describe("main", () => {
-  beforeAll( async () => {
-    await nano.db.destroy('datum').catch((err) => undefined);
-  });
-  
-  beforeEach( async () => {
-    await nano.db.create('datum')
-    .catch((err) => undefined);
+  beforeAll(async () => {
+    await nano.db.destroy("datum").catch((err) => undefined);
   });
 
-  afterEach( async () => {
-    await nano.db.destroy('datum').catch(err => undefined);
+  beforeEach(async () => {
+    await nano.db.create("datum").catch((err) => undefined);
+  });
+
+  afterEach(async () => {
+    await nano.db.destroy("datum").catch((err) => undefined);
   });
 
   it("inserts documents into couchdb", async () => {
     await main();
 
-    const db = nano.use('datum');
+    const db = nano.use("datum");
     await db.info().then((info) => {
       expect(info.doc_count).toEqual(1);
     });
-
   });
 
   it("creates the database if it doesn't exist", async () => {
-    await nano.db.destroy('datum').catch(() => {});
-    
+    await nano.db.destroy("datum").catch(() => {});
+
     await main();
-    nano.db.list().then(body => {
-      expect(body.includes ('datum'));
+    nano.db.list().then((body) => {
+      expect(body.includes("datum"));
     });
   });
 });
