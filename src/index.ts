e14650482c1541e422f1f0b4d6f22e37b0513389
Nate Faber <nathaniel@njfaber.com>
2020-09-07 12:45:04 +0200

    Create blank documents in couchdb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
 function main() {
-    console.log("Hello World!")
+    console.log("Hello World!");
+    const nano = require('nano')('http://localhost:5984');
+    const db = nano.use("datum");
+
+    const _id = new Date().toISOString();
+
+    db.insert({}, _id).catch((err: any) => console.log(err))
 }
 main();
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,23 @@
 const main = require('../src/index');
+const nock = require('nock');
+const chai = require('chai');
+const chaiNock = require('chai-nock');
+
+chai.use(chaiNock);
+
+const couchNock = nock('http://localhost:5984')
+.post('/datum')
+.reply(200, (uri, requestBody) => ({
+    "ok": true,
+    "id": requestBody["_id"] || "nock_generated_id",
+    "rev": "nock_generated_revision"
+}));
 
 describe("main", () => {
+    beforeEach(() => {
+        nock.cleanAll();
+    });
+
     test("main logs hello world", () => {
         const mockLog = jest.fn();
         console.log = mockLog;
@@ -9,5 +26,11 @@ describe("main", () => {
 
         expect(mockLog).toBeCalledTimes(1);
         expect(mockLog.mock.calls[0][0]).toEqual("Hello World!");
-    })
+    });
+
+    it("makes an entry into couchdb", () => {
+        main();
+
+        expect(couchNock.isDone()).toEqual(true);
+    });
 });
