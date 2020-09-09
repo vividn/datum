2b7fdb69a0fda336e5661d7541b9b553126e8f4a
Nate Faber <nathaniel@njfaber.com>
2020-09-09 23:16:30 +0200

    Make error catching look a little nicer

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,18 @@
 const main = require("../src/index");
 const nano = require("nano")("http://admin:password@localhost:5983");
+const pass = () => {};
 
 describe("main", () => {
   beforeAll(async () => {
-    await nano.db.destroy("datum").catch((err) => undefined);
+    await nano.db.destroy("datum").catch(pass);
   });
 
   beforeEach(async () => {
-    await nano.db.create("datum").catch((err) => undefined);
+    await nano.db.create("datum").catch(pass);
   });
 
   afterEach(async () => {
-    await nano.db.destroy("datum").catch((err) => undefined);
+    await nano.db.destroy("datum").catch(pass);
   });
 
   it("inserts documents into couchdb", async () => {
@@ -24,7 +25,7 @@ describe("main", () => {
   });
 
   it("creates the database if it doesn't exist", async () => {
-    await nano.db.destroy("datum").catch(() => {});
+    await nano.db.destroy("datum").catch(pass);
 
     await main();
     nano.db.list().then((body) => {
