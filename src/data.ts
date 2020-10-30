2fde5abb1ae96c5e6a25a44ad93396d6cd89135b
Nate Faber <nathaniel@njfaber.com>
2020-10-30 14:04:06 +0100

    Move parsing of withKey in the original reduce call

--- src/data.ts
+++ src/data.ts
@@ -10,55 +10,53 @@ const parseData = function ({
 }: parseDataType) {
   const payload: { [key: string]: any } = {};
 
-  // split into arguments that come with a key "key=value", and those without "value"
-  const [withKey, withoutKey] = posArgs.reduce(
-    (result, element) => {
-      if (typeof element === "string" && element.includes("=")) {
-        result[0].push(element);
-      } else {
-        result[1].push(element);
-      }
+  // save the arguments with explicit keys into the payload leaving only the keyless
+  const withoutKey = posArgs.reduce((result, arg) => {
+    if (typeof arg === "string" && arg.includes("=")) {
+      const [key, ...eqSepValue] = arg.split("=");
+      payload[key] = inferType(eqSepValue.join("="));
       return result;
-    },
-    [[] as string[], [] as (string | number)[]]
-  );
-
-  for (const arg of withKey) {
-    const [key, ...eqSepValue] = arg.split("=");
-    payload[key] = inferType(eqSepValue.join('='));
-  }
+    }
+    result.push(arg);
+    return result;
+  }, [] as (string | number)[]);
 
   if (withoutKey.length > 0) {
     if (lenient) {
       payload["extraData"] = withoutKey.map((arg) => {
-        return inferType(arg)});
+        return inferType(arg);
+      });
     } else {
-      throw new DataError('some data do not have keys. Assign keys with equals signs `key=value`, key args `-K key1 -K key2 value1 value2`, or use --lenient');
+      throw new DataError(
+        "some data do not have keys. Assign keys with equals signs `key=value`, key args `-K key1 -K key2 value1 value2`, or use --lenient"
+      );
     }
   }
 
   return payload;
 };
 
-const inferType = (value: (number | string)) => {
-  if (typeof(value) === "number") {
-    return value
+const inferType = (value: number | string) => {
+  if (typeof value === "number") {
+    return value;
   }
-  if(/^null$/i.test(value)) {
-    return null
+  if (/^null$/i.test(value)) {
+    return null;
   }
-  if(/^nan$/i.test(value)) {
-    return Number.NaN
+  if (/^nan$/i.test(value)) {
+    return Number.NaN;
   }
-  if(/^-?inf(inity)?/i.test(value)) {
-    return value[0] === '-' ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY
+  if (/^-?inf(inity)?/i.test(value)) {
+    return value[0] === "-"
+      ? Number.NEGATIVE_INFINITY
+      : Number.POSITIVE_INFINITY;
   }
   try {
     const RJSON = require("relaxed-json");
     return RJSON.parse(value);
   } catch {}
   return value;
-}
+};
 
 class DataError extends Error {
   constructor(message: string) {
