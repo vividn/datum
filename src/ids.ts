9a134737859e0558a57e6ba1b478c6cfa280eb4f
Nate Faber <nathaniel@njfaber.com>
2020-11-10 08:09:19 +0100

    Retrieve deeper values for id

--- src/ids.ts
+++ src/ids.ts
@@ -9,48 +9,70 @@ const assembleId = function ({
   partitionField?: string | string[];
   payload: { [key: string]: any };
 }): string {
-  const idSection = buildDelimitedFromFields({fields: idField, delimiter: idDelimiter, payload});
-  const partitionSection = buildDelimitedFromFields({fields: partitionField, delimiter: idDelimiter, payload});
+  const idSection = buildDelimitedFromFields({
+    fields: idField,
+    delimiter: idDelimiter,
+    payload,
+  });
+  const partitionSection = buildDelimitedFromFields({
+    fields: partitionField,
+    delimiter: idDelimiter,
+    payload,
+  });
 
-  const fullId = partitionSection.length > 0 ? `${partitionSection}:${idSection}` : idSection
+  const fullId =
+    partitionSection.length > 0
+      ? `${partitionSection}:${idSection}`
+      : idSection;
   return fullId;
 };
 
 const buildDelimitedFromFields = function ({
   fields,
   delimiter,
-  payload
-}:{
+  payload,
+}: {
   fields: string | string[];
   delimiter: string;
   payload: { [key: string]: any };
 }) {
   const arrFields = typeof fields === "string" ? [fields] : fields;
-  const allComponents = arrFields.reduce((result: string[], fieldName: string) => {
-    const splitNonEscapedQuotes = fieldName
-      .replace(/(?<!\\)'/g, "\xff\x00")
-      .replace(/\\'/g, "'")
-      .split("\xff\x00");
+  const allComponents = arrFields.reduce(
+    (result: string[], fieldName: string) => {
+      const splitNonEscapedQuotes = fieldName
+        .replace(/(?<!\\)'/g, "\xff\x00")
+        .replace(/\\'/g, "'")
+        .split("\xff\x00");
 
-    // field names are even numbered indices, raw strings are odd
-    const joinedRawStringsAndFields = splitNonEscapedQuotes
-      .reduce((combined: string[], value: string, index: number) => {
-        if (index % 2 === 1) {
-          combined.push(value);
-        } else {
-          if (value in payload) {
-            combined.push(payload[value]);
+      // field names are even numbered indices, raw strings are odd
+      const joinedRawStringsAndFields = splitNonEscapedQuotes
+        .reduce((combined: string[], strPart: string, index: number) => {
+          if (index % 2 === 1) {
+            combined.push(strPart);
+          } else {
+            // retrieve deeper values with dot notation
+            const extractedValue = strPart
+              .split(".")
+              .reduce((o, i) => o[i], payload);
+            if (extractedValue !== undefined) {
+              const valueAsString =
+                typeof extractedValue === "string"
+                  ? extractedValue
+                  : JSON.stringify(extractedValue);
+              combined.push(valueAsString);
+            }
           }
-        }
-        return combined;
-      }, [])
-      .join("");
+          return combined;
+        }, [])
+        .join("");
 
-    result.push(joinedRawStringsAndFields);
-    return result;
-  }, []);
+      result.push(joinedRawStringsAndFields);
+      return result;
+    },
+    []
+  );
 
-  return allComponents.join(delimiter)
-}
+  return allComponents.join(delimiter);
+};
 
 module.exports = { assembleId };
--- test/ids.test.js
+++ test/ids.test.js
@@ -10,68 +10,129 @@ const testPayload = {
   foo: "abc",
   bar: "def",
   complex: { data: "structure" },
-  "wei'rd": "da'ta"
+  array: ["various", 2, "data"],
+  num: 3,
+  "wei'rd": "da'ta",
 };
 
+const testPayloadWithPartition = { ...testPayload, field: "main" };
+
 const expectAssembleIdReturns = (props, expectedReturn) => {
-  expect(assembleId({ payload: testPayload, ...props }), JSON.stringify(props)).toBe(expectedReturn);
+  expect(
+    assembleId({ payload: testPayload, ...props }),
+    JSON.stringify(props)
+  ).toBe(expectedReturn);
 };
 
 describe("assembleId", () => {
   it("returns the occurTime as a default id", () => {
-    expectAssembleIdReturns({}, testPayload.meta.occurTime)
-  })
+    expectAssembleIdReturns({}, testPayload.meta.occurTime);
+  });
 
   it("can assemble single component ids", () => {
     expectAssembleIdReturns({ idField: "foo" }, "abc");
-    expectAssembleIdReturns({ idField: "field" }, "main");
     expectAssembleIdReturns({ idField: "bar" }, "def");
   });
 
   it("interprets single quotes as raw strings", () => {
     expectAssembleIdReturns({ idField: "'foo'" }, "foo");
     expectAssembleIdReturns({ idField: "'manual_raw$id'" }, "manual_raw$id");
-    expectAssembleIdReturns({ idField: "'missing_ending_quote_ignored" }, "missing_ending_quote_ignored");
+    expectAssembleIdReturns(
+      { idField: "'missing_ending_quote_ignored" },
+      "missing_ending_quote_ignored"
+    );
   });
 
   it("can interpolate raw strings and field names", () => {
     expectAssembleIdReturns({ idField: "'foo'foo" }, "fooabc");
-    expectAssembleIdReturns({ idField: "foo'_:)_'bar"}, "abc_:)_def");
+    expectAssembleIdReturns({ idField: "foo'_:)_'bar" }, "abc_:)_def");
     expectAssembleIdReturns({ idField: "foo'raw" }, "abcraw");
-    expectAssembleIdReturns({ idField: "foo''bar"}, "abcdef")
+    expectAssembleIdReturns({ idField: "foo''bar" }, "abcdef");
   });
 
   it("escapes single quotes properly", () => {
-    expectAssembleIdReturns({ idField: "'raw with \\'escaped quotes\\''"}, "raw with 'escaped quotes'")
-    expectAssembleIdReturns({ idField: "wei\\'rd"}, "da'ta")
-  })
+    expectAssembleIdReturns(
+      { idField: "'raw with \\'escaped quotes\\''" },
+      "raw with 'escaped quotes'"
+    );
+    expectAssembleIdReturns({ idField: "wei\\'rd" }, "da'ta");
+  });
 
   it("combines multiple components with the id_delimiter", () => {
     expectAssembleIdReturns({ idField: ["foo", "bar"] }, "abc__def");
-    expectAssembleIdReturns({ idField: ["foo", "bar"], idDelimiter: "@"}, "abc@def")
-    expectAssembleIdReturns({ idField: ["foo", "bar"], idDelimiter: ""}, "abcdef")
-    expectAssembleIdReturns({ idField: ["foo", "'raw'"]}, "abc__raw")
+    expectAssembleIdReturns(
+      { idField: ["foo", "bar"], idDelimiter: "@" },
+      "abc@def"
+    );
+    expectAssembleIdReturns(
+      { idField: ["foo", "bar"], idDelimiter: "" },
+      "abcdef"
+    );
+    expectAssembleIdReturns({ idField: ["foo", "'raw'"] }, "abc__raw");
+  });
+
+  it("omits fields that do not exist", () => {
+    expectAssembleIdReturns({ idField: "notAField" }, "");
+    expectAssembleIdReturns(
+      { idField: ["foo", "notAField", "bar"] },
+      "abc____def"
+    );
   });
 
   it("can retrieve deeper values", () => {
-    
-  })
+    expectAssembleIdReturns(
+      { idField: "meta.createTime" },
+      testPayload.meta.createTime
+    );
+    expectAssembleIdReturns({ idField: "complex.data" }, "structure");
+    expectAssembleIdReturns({ idField: ["foo", "complex.notAKey"]}, "abc__")
+    expectAssembleIdReturns({ idField: "not.real.keys"}, "")
+  });
+
+  it("serializes numbers, objects, and arrays", () => {
+    expectAssembleIdReturns({ idField: "num" }, "3");
+    expectAssembleIdReturns({ idField: "complex" }, '{"data":"structure"}');
+    expectAssembleIdReturns({ idField: "array"}, '["various",2,"data"]')
+  });
 
   it("prepends the partition field if provided", () => {
-    expectAssembleIdReturns({ payload: {...testPayload, field: "main"}}, "main:" + testPayload.meta.occurTime)
-    expectAssembleIdReturns({ idField: "foo", payload: {...testPayload, field: "otherName"}}, "otherName:abc")
-    expectAssembleIdReturns({ payload: {field: "onlyField"}}, "onlyField:")
-  })
+    expectAssembleIdReturns(
+      { payload: testPayloadWithPartition },
+      "main:" + testPayload.meta.occurTime
+    );
+    expectAssembleIdReturns(
+      { idField: "foo", payload: { ...testPayload, field: "otherName" } },
+      "otherName:abc"
+    );
+    expectAssembleIdReturns({ payload: { field: "onlyField" } }, "onlyField:");
+  });
 
   it("can use other fields, strings, and combinations as partition", () => {
-    expectAssembleIdReturns({ partitionField: "foo", idField: "bar" }, "abc:def")
-    expectAssembleIdReturns({ partitionField: "bar", idField: "bar"}, "def:def")
-    expectAssembleIdReturns({ partitionField: "'rawString'", idField: ["foo", "'raw'"]}, "rawString:abc__raw")
-    expectAssembleIdReturns({ partitionField: ["foo", "bar'-with-extra'"], idField: "'id'"}, "abc__def-with-extra:id")
-    expectAssembleIdReturns({ partitionField: ["foo", "bar"], idField: ["'some'", "'strings'"], idDelimiter: "!"}, "abc!def:some!strings")
-  })
-
-  
+    expectAssembleIdReturns(
+      { partitionField: "foo", idField: "bar" },
+      "abc:def"
+    );
+    expectAssembleIdReturns(
+      { partitionField: "bar", idField: "bar" },
+      "def:def"
+    );
+    expectAssembleIdReturns(
+      { partitionField: "'rawString'", idField: ["foo", "'raw'"] },
+      "rawString:abc__raw"
+    );
+    expectAssembleIdReturns(
+      { partitionField: ["foo", "bar'-with-extra'"], idField: "'id'" },
+      "abc__def-with-extra:id"
+    );
+    expectAssembleIdReturns(
+      {
+        partitionField: ["foo", "bar"],
+        idField: ["'some'", "'strings'"],
+        idDelimiter: "!",
+      },
+      "abc!def:some!strings"
+    );
+  });
 
   it("handles this example", () => {
     expectAssembleIdReturns(
