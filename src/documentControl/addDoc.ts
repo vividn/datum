6a0a8498e5d046c40e02d7a3551e961944c8a640
Nate Faber <nathaniel@njfaber.com>
2021-06-22 21:20:58 +0200

    Implement DatumPayload part of addDoc

--- src/documentControl/addDoc.ts
+++ src/documentControl/addDoc.ts
@@ -7,20 +7,35 @@ import {
   isDatumPayload,
 } from "./DatumDocument";
 import { DocumentScope } from "nano";
+import { DateTime } from "luxon";
+import { assembleId } from "../ids";
+import { IdError } from "../errors";
 
 type addDocType = {
   db: DocumentScope<DatumPayload | DataOnlyPayload>;
   payload: DatumPayload | DataOnlyPayload;
-  id?: string;
 };
 
 const addDoc = async ({
   db,
   payload,
-  id,
 }: addDocType): Promise<DatumDocument | DataOnlyDocument> => {
   if (isDatumPayload(payload)) {
-    return payload as DatumDocument;
+    const now = DateTime.utc.toString();
+    payload.meta.createTime = now;
+    payload.meta.modifyTime = now;
+
+    const id = payload.meta.idStructure
+      ? assembleId({ data: payload.data, meta: payload.meta })
+      : payload._id ?? undefined;
+    if (id === undefined) {
+      throw new IdError("id could not be determined");
+    }
+    payload._id = id;
+
+    await db.insert(payload);
+    const addedDoc = await db.get(id) as DatumDocument;
+    return addedDoc;
   } else {
     return payload as DataOnlyDocument;
   }
