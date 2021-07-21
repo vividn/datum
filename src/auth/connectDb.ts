0ed669494a0f9afb9c9f8d4141d7432da1fb408d
Nate Faber <nathaniel@njfaber.com>
2021-07-21 23:33:45 +0200

    Merge pull request #4
    * Make yargs strict
    
    * Upgrade yargs
    
    * Create add command on yargs
    
    * Separate out add command into separate file
    
    * WIP: keep moving stuff around
    
    * keep moving stuff around
    
    * Use handler function in yargs to handle the add command
    
    * Add TODOs
    
    * Fix types in tests
    
    * Fix wrong regex
    
    * Add todo for integration tests
    
    * Remove integration test file again
    
    * Use separate db as datum for addCmd testing
    
    * Lint code

---
+++ src/auth/connectDb.ts
@@ -0,0 +1,28 @@
+import Nano, { DocumentScope } from "nano";
+import { EitherPayload } from "../documentControl/DatumDocument";
+import dotenv from "dotenv";
+import { BaseDatumArgs } from "../input";
+
+const connectDb = (args: BaseDatumArgs): DocumentScope<EitherPayload> => {
+  if (args.env !== undefined) {
+    dotenv.config({ path: args.env });
+  }
+  const env = process.env.NODE_ENV || "production";
+  const defaultHost =
+    env === "production" ? "localhost:5984" : "localhost:5983";
+  const couchConfig = {
+    username: args.username ?? process.env.COUCHDB_USER ?? "admin",
+    password: args.password ?? process.env.COUCHDB_PASSWORD ?? "password",
+    hostname: args.host ?? process.env.COUCHDB_HOSTNAME ?? defaultHost,
+  };
+  const nano = Nano(
+    `http://${couchConfig.username}:${couchConfig.password}@${couchConfig.hostname}`
+  );
+
+  const { db: dbName = "datum" } = args;
+
+  const db: DocumentScope<EitherPayload> = nano.use(dbName);
+  return db;
+};
+
+export default connectDb;
--- test/addCmd.test.ts
+++ test/addCmd.test.ts
@@ -8,55 +8,50 @@ import {
   jest,
 } from "@jest/globals";
 import { pass, testNano } from "./test-utils";
-import { main } from "../src";
 import { BaseDataError } from "../src/errors";
-import { DatumDocument } from "../src/documentControl/DatumDocument";
+import {
+  DatumDocument,
+  EitherPayload,
+} from "../src/documentControl/DatumDocument";
+import addCmd from "../src/commands/addCmd";
+import * as connectDb from "../src/auth/connectDb";
+import { DocumentScope } from "nano";
 
-const nano = testNano;
 const originalLog = console.log;
 
-describe("main", () => {
+describe("addCmd", () => {
   const mockedLog = jest.fn();
-  const db = nano.use("datum");
+  const dbName = "add_cmd_test";
+  const db = testNano.use(dbName) as DocumentScope<EitherPayload>;
+  jest.spyOn(connectDb, "default").mockImplementation(() => db);
 
   beforeAll(async () => {
-    await nano.db.destroy("datum").catch(pass);
+    await testNano.db.destroy(dbName).catch(pass);
   });
 
   beforeEach(async () => {
-    await nano.db.create("datum").catch(pass);
+    await testNano.db.create(dbName).catch(pass);
     console.log = mockedLog;
   });
 
   afterEach(async () => {
-    await nano.db.destroy("datum").catch(pass);
+    await testNano.db.destroy(dbName).catch(pass);
     console.log = originalLog;
     mockedLog.mockReset();
   });
 
   it("inserts documents into couchdb", async () => {
-    await main({});
+    await addCmd({});
 
-    const db = nano.use("datum");
     await db.info().then((info) => {
       expect(info.doc_count).toEqual(1);
     });
   });
 
-  it("creates the database if it doesn't exist", async () => {
-    await nano.db.destroy("datum").catch(pass);
-
-    await main({});
-    nano.db.list().then((body) => {
-      expect(body.includes("datum"));
-    });
-  });
-
   it("can undo adding documents with a known id", async () => {
-    await main({ idPart: "this_one_should_be_deleted" });
-    await main({ idPart: "kept" });
+    await addCmd({ idPart: "this_one_should_be_deleted" });
+    await addCmd({ idPart: "kept" });
 
-    const db = nano.use("datum");
     await db.info().then((info) => {
       expect(info.doc_count).toEqual(2);
     });
@@ -64,7 +59,7 @@ describe("main", () => {
     await db.get("kept");
 
     mockedLog.mockReset();
-    await main({ idPart: "this_one_should_be_deleted", undo: true });
+    await addCmd({ idPart: "this_one_should_be_deleted", undo: true });
 
     expect(mockedLog).toHaveBeenCalledWith(expect.stringContaining("DELETE"));
     await db.info().then((info) => {
@@ -79,25 +74,25 @@ describe("main", () => {
   it("undoes a document with a time in the past if it contains occurTime", async () => {
     const now = "2021-06-28T06:30:00.000Z";
     const inAMinute = "2021-06-28T06:31:00.000Z";
-    await main({ time: now });
+    await addCmd({ time: now });
     const insertedDoc = (await db.get(now)) as DatumDocument;
     expect(insertedDoc.meta.idStructure).toMatch(/%?occurTime%/);
 
-    await main({ time: inAMinute, undo: true });
+    await addCmd({ time: inAMinute, undo: true });
     await expect(db.get(now)).rejects.toThrowError("deleted");
   });
 
   // TODO: Make undo system more robust and more tested
 
   it("Can remove metadata entirely", async () => {
-    expect(await main({ idPart: "hasMetadata" })).toHaveProperty("meta");
+    expect(await addCmd({ idPart: "hasMetadata" })).toHaveProperty("meta");
     expect(
-      await main({ idPart: "noMeta", noMetadata: true })
+      await addCmd({ idPart: "noMeta", noMetadata: true })
     ).not.toHaveProperty("meta");
   });
 
   it("tells the user if the document already exists", async () => {
-    await main({ idPart: "my name is bob" });
+    await addCmd({ idPart: "my name is bob" });
     expect(mockedLog).toHaveBeenCalledWith(expect.stringContaining("CREATE"));
     expect(mockedLog).not.toHaveBeenCalledWith(
       expect.stringContaining("EXISTS")
@@ -105,7 +100,7 @@ describe("main", () => {
 
     mockedLog.mockReset();
 
-    await main({ idPart: "my name is bob" });
+    await addCmd({ idPart: "my name is bob" });
     expect(mockedLog).not.toHaveBeenCalledWith(
       expect.stringContaining("CREATE")
     );
@@ -114,7 +109,7 @@ describe("main", () => {
 
   it("inserts id structure into the metadata", async () => {
     expect(
-      await main({ idPart: ["rawString", "%foo%!!"], _: ["foo=abc"] })
+      await addCmd({ idPart: ["rawString", "%foo%!!"], data: ["foo=abc"] })
     ).toMatchObject({
       meta: { idStructure: "rawString__%foo%!!" },
     });
@@ -122,13 +117,13 @@ describe("main", () => {
 
   it("can use custom base data", async () => {
     expect(
-      await main({ baseData: "{a: 1, b:2, c:3 }", idPart: "basedata-doc1" })
+      await addCmd({ baseData: "{a: 1, b:2, c:3 }", idPart: "basedata-doc1" })
     ).toMatchObject({ data: { a: 1, b: 2, c: 3 }, _id: "basedata-doc1" });
   });
 
   it("can write payloads directly by specifying base-data and no-metadata", async () => {
     expect(
-      await main({
+      await addCmd({
         noMetadata: true,
         baseData: "{a: 1, b:2, c:3}",
         idPart: "basedata-doc2",
@@ -142,44 +137,44 @@ describe("main", () => {
   });
 
   it("throws a BaseDataError if baseData is malformed", async () => {
-    await expect(main({ baseData: "string" })).rejects.toThrowError(
+    await expect(addCmd({ baseData: "string" })).rejects.toThrowError(
       BaseDataError
     );
   });
 
   it("prefers the _id specified when in no-metadata mode", async () => {
     expect(
-      await main({
+      await addCmd({
         noMetadata: true,
         baseData: "{ _id: payload-id }",
         idPart: "argument-id",
       })
     ).toMatchObject({ _id: "payload-id" });
     expect(
-      await main({
+      await addCmd({
         noMetadata: true,
         baseData: "{ _id: payload-id-2 }",
         idPart: "%keyId%",
-        _: ["keyId=key-id"],
+        data: ["keyId=key-id"],
       })
     ).toMatchObject({ _id: "payload-id-2" });
     expect(
-      await main({
+      await addCmd({
         noMetadata: true,
-        _: ["_id=posArgs-id"],
+        data: ["_id=posArgs-id"],
         idPart: "idPart-id",
       })
     ).toMatchObject({ _id: "posArgs-id" });
   });
 
   it("does not contain idStructure in the metadata if id does not depend on values from data", async () => {
-    const returnDoc = (await main({ idPart: "notAField" })) as DatumDocument;
+    const returnDoc = (await addCmd({ idPart: "notAField" })) as DatumDocument;
     expect(returnDoc._id).toBe("notAField");
     expect(returnDoc.meta).not.toHaveProperty("idStructure");
   });
 
   it("contains random identifiers in the metadata", async () => {
-    const doc = await main({});
+    const doc = await addCmd({});
     const { random, humanId } = doc?.meta;
 
     expect(random).toBeGreaterThanOrEqual(0);
@@ -189,14 +184,14 @@ describe("main", () => {
 
   it("can display just the data of documents or the whole documents", async () => {
     const matchExtraKeysInAnyOrder = /^(?=[\s\S]*_id:)(?=[\s\S]*data:)(?=[\s\S]*meta:)/;
-    await main({ idPart: "this-id" });
+    await addCmd({ idPart: "this-id" });
     expect(mockedLog).not.toHaveBeenCalledWith(
       expect.stringMatching(matchExtraKeysInAnyOrder)
     );
 
     mockedLog.mockClear();
 
-    await main({ idPart: "that-id", showAll: true });
+    await addCmd({ idPart: "that-id", showAll: true });
     expect(mockedLog).toHaveBeenCalledWith(
       expect.stringMatching(matchExtraKeysInAnyOrder)
     );
