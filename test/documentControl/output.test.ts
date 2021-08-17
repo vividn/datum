169c34d4e021df76b77ee29a93b0dae888b964b8
Nate Faber <nathaniel@njfaber.com>
2021-08-17 21:36:35 +0200

    write more tests

--- test/documentControl/output.test.ts
+++ test/documentControl/output.test.ts
@@ -9,6 +9,7 @@ import {
 import updateDoc from "../../src/documentControl/updateDoc";
 import { pass, testNano } from "../test-utils";
 import { EitherPayload } from "../../src/documentControl/DatumDocument";
+import addDoc from "../../src/documentControl/addDoc";
 
 const dbName = "doc_control_output_test";
 const db = testNano.db.use<EitherPayload>(dbName);
@@ -30,10 +31,20 @@ afterEach(async () => {
   console.log = originalLog;
 });
 
-test.todo("addDoc displays a CREATE: message and the document if showOutput");
-test.todo(
-  "addDoc displays an EXISTS: message and the document if showOuput and conlfict"
-);
+test("addDoc displays a CREATE: message and the document if showOutput", async () => {
+  await addDoc({ db, payload: { abc: "def" }, showOutput: true });
+  expect(mockedLog).toHaveBeenCalledWith(expect.stringContaining("CREATE"));
+});
+
+test("addDoc displays an EXISTS: message and the document if showOuput and conlfict", async () => {
+  await db.insert({ _id: "alreadyHere", foo: "bar" });
+  await addDoc({
+    db,
+    payload: { _id: "alreadyHere", baz: "bazzy" },
+    showOutput: true,
+  }).catch(pass);
+  expect(mockedLog).toHaveBeenCalledWith(expect.stringContaining("EXISTS"));
+});
 
 test("updateDoc outputs an UPDATE: message if showOutput is true", async () => {
   await db.insert({ _id: "name", foo: "bar" });
@@ -41,16 +52,13 @@ test("updateDoc outputs an UPDATE: message if showOutput is true", async () => {
   expect(mockedLog).toHaveBeenCalledWith(expect.stringContaining("UPDATE"));
 });
 
-test.todo(
-  "updateDoc outputs a NODIFF: message if showing output and no update needed",
-  async () => {
-    await db.insert({ _id: "name", foo: "bar" });
-    await updateDoc({
-      db,
-      id: "name",
-      payload: { foo2: "abc2" },
-      updateStrategy: "useOld",
-    });
-    expect(mockedLog).toHaveBeenCalledWith(expect.stringContaining("NODIFF"));
-  }
-);
+test("updateDoc outputs a NODIFF: message if showing output and no update needed", async () => {
+  await db.insert({ _id: "name", foo: "bar" });
+  await updateDoc({
+    db,
+    id: "name",
+    payload: { foo2: "abc2" },
+    updateStrategy: "useOld",
+  });
+  expect(mockedLog).toHaveBeenCalledWith(expect.stringContaining("NODIFF"));
+});
