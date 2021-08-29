699e1e82e9f848d3520e701b9693d348f837cf58
Nate Faber <nathaniel@njfaber.com>
2021-08-29 15:35:17 +0200

    Add in some types

--- src/views/getViewDoc.ts
+++ src/views/getViewDoc.ts
@@ -1,11 +1,27 @@
 import { DocumentScope, ViewDocument } from "nano";
+import { EitherDocument } from "../documentControl/DatumDocument";
 
-type GetViewDocType = {
+type GetViewDocType = {};
 
-}
+export async function getViewDoc() {}
 
-export async function getViewDoc () {}
-
-export function asViewDb<T> (db: DocumentScope<T>): DocumentScope<ViewDocument<T>> {
+export function asViewDb<T>(
+  db: DocumentScope<T>
+): DocumentScope<ViewDocument<T>> {
   return db as unknown as DocumentScope<ViewDocument<T>>;
 }
+
+export type DatumView<D extends EitherDocument> = {
+  name: string;
+  map: MapFunction<D>;
+  reduce?: ReduceFunction | { [viewName: string]: ReduceFunction };
+};
+
+export type ReduceFunction =
+  | ((keysAndDocIds: [any, string][], values: [], rereduce: boolean) => any)
+  | "_sum"
+  | "_stats"
+  | "_count"
+  | "_approx_count_distinct"
+  | string;
+export type MapFunction<D extends EitherDocument> = string | ((doc: D) => void);
