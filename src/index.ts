6498241afc8cda2ad49294e5fc01523215e1023f
Nate Faber <nathaniel@njfaber.com>
2021-09-01 22:38:42 +0200

    Add skeleton for map command

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@
 import { baseYargs } from "./input/baseYargs";
 import { DocExistsError } from "./documentControl/base";
 import addCmd, { AddCmdArgs } from "./commands/addCmd";
+import mapCmd, { MapCmdArgs } from "./commands/mapCmd";
 
 export async function main(cliInput: string | string[]): Promise<void> {
-  const args = await baseYargs.parseAsync(cliInput);
+  const args = await baseYargs.parse(cliInput);
   switch (args._[0]) {
     case "add":
-      await addCmd(args as AddCmdArgs);
+      await addCmd(args as unknown as AddCmdArgs);
+      break;
+    case "map":
+      await mapCmd(args as unknown as MapCmdArgs);
       break;
     default:
       throw Error("command not recognized");
