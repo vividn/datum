7f025ebe13d285daf3fcdeb0548b5450c6c8fc7d
Nate Faber <nathaniel@njfaber.com>
2021-09-04 19:52:59 +0200

    Fix test

--- src/views/setupDatumViews.ts
+++ src/views/setupDatumViews.ts
@@ -2,14 +2,14 @@ import insertDatumView from "./insertDatumView";
 import { BaseDocControlArgs } from "../documentControl/base";
 import getAllDatumViews from "./getAllDatumViews";
 
-const allDatumViews = getAllDatumViews();
 
 export default async function setupDatumViews({
   db,
   show,
 }: BaseDocControlArgs): Promise<void> {
+  const allDatumViews = getAllDatumViews();
   const promises = allDatumViews.map((datumView) => {
-    insertDatumView({ datumView, db, show });
+    return insertDatumView({ datumView, db, show });
   });
   await Promise.all(promises);
   return;
