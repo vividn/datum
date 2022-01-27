a13d087c262da9a891a3ad243a5a959bc8265836
Nate Faber <nathaniel@njfaber.com>
2022-01-27 15:11:40 +0100

    Add force undo

--- src/commands/__test__/addCmd.test.ts
+++ src/commands/__test__/addCmd.test.ts
@@ -19,7 +19,7 @@ import * as addDoc from "../../documentControl/addDoc";
 import { DocumentScope } from "nano";
 import { DocExistsError } from "../../documentControl/base";
 import { Show } from "../../output/output";
-import { DateTime, Duration, Settings } from "luxon";
+import { DateTime, Settings } from "luxon";
 
 const originalLog = console.log;
 
@@ -65,42 +65,6 @@ describe("addCmd", () => {
       payload: { data: { foo: "abc" }, meta: { idStructure: "%foo%" } },
     });
   });
-
-  it("can undo adding documents with a known id", async () => {
-    await addCmd({ idPart: "this_one_should_be_deleted" });
-    await addCmd({ idPart: "kept" });
-
-    await db.info().then((info) => {
-      expect(info.doc_count).toEqual(2);
-    });
-    await db.get("this_one_should_be_deleted");
-    await db.get("kept");
-
-    mockedLog.mockReset();
-    await addCmd({ idPart: "this_one_should_be_deleted", undo: true });
-
-    expect(mockedLog).toHaveBeenCalledWith(expect.stringContaining("DELETE"));
-    await db.info().then((info) => {
-      expect(info.doc_count).toEqual(1);
-    });
-    await db.get("kept");
-    await expect(db.get("this_one_should_be_deleted")).rejects.toThrowError(
-      "deleted"
-    );
-  });
-
-  it("undoes a document with a time in the past if it contains occurTime", async () => {
-    const now = "2021-06-28T06:30:00.000Z";
-    const inAMinute = "2021-06-28T06:31:00.000Z";
-    await addCmd({ time: now });
-    const insertedDoc = (await db.get(now)) as DatumDocument;
-    expect(insertedDoc.meta.idStructure).toMatch(/%occurTime%/);
-
-    await addCmd({ time: inAMinute, undo: true });
-    await expect(db.get(now)).rejects.toThrowError("deleted");
-  });
-
-  // TODO: Make undo system more robust and more tested
 
   it("Can remove metadata entirely", async () => {
     expect(await addCmd({ idPart: "hasMetadata" })).toHaveProperty("meta");
@@ -332,26 +296,6 @@ describe("addCmd", () => {
     expect(newDoc.meta.utcOffset).toEqual(-7);
 
     Settings.defaultZone = "system";
-    Settings.resetCaches();
-  });
-
-  it("prevents undo if created more than 15 minutes ago", async () => {
-    const mockNow = DateTime.utc(2020, 5, 10, 15, 25, 30);
-    const oldTime = mockNow.minus(
-      Duration.fromObject({ minutes: 15, seconds: 30 })
-    );
-    Settings.now = () => mockNow.toMillis();
-
-    await db.insert({
-      _id: "oldDoc",
-      data: {},
-      meta: { createTime: oldTime.toString() },
-    });
-
-    await expect(addCmd({ idPart: "oldDoc", undo: true })).rejects.toThrowError(
-      "Doc created more than fifteen minutes ago"
-    );
-
     Settings.resetCaches();
   });
 });
