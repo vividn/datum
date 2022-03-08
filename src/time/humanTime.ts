7c21de43d7beee4fabd701b49f4da1a3decbfee4
Nate Faber <nathaniel@njfaber.com>
2022-03-08 17:37:44 +0100

    Add utilities to get occurTime out and formatted (#209)
    * add basic strucutre and tests for humanTime
    
    * Write tests
    
    * Write humanTime
    
    * Use humanTime for tailCmd
    
    * Add functions for extracting occurTime from documents

---
+++ src/time/humanTime.ts
@@ -0,0 +1,25 @@
+import { DateTime, Duration } from "luxon";
+
+export function humanTime(time: DateTime): string {
+  let dateTimeFormat;
+  const now = DateTime.now();
+  const yesterday = now.minus(Duration.fromISO("P1D"));
+  const tomorrow = now.plus(Duration.fromISO("P1D"));
+  if (now.hasSame(time, "day")) {
+    dateTimeFormat = "HH:mm:ss";
+  } else if (yesterday.hasSame(time, "day")) {
+    dateTimeFormat = "'-1d,' HH:mm:ss";
+  } else if (tomorrow.hasSame(time, "day")) {
+    dateTimeFormat = "'+1d,' HH:mm:ss";
+  } else if (now.hasSame(time, "year")) {
+    dateTimeFormat = "MMM d, HH:mm:ss";
+  } else {
+    dateTimeFormat = "yyyy-MM-dd, HH:mm:ss";
+  }
+
+  if (now.offset !== time.offset) {
+    dateTimeFormat += " 'UTC'Z";
+  }
+
+  return time.toFormat(dateTimeFormat);
+}
