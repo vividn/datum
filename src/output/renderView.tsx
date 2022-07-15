9329f45940272ed18b87f33271ccb3f528be35c3
Nate Faber <nathaniel@njfaber.com>
2022-07-15 17:51:10 +0200

    Remove lines in map table

--- src/output/renderView.tsx
+++ src/output/renderView.tsx
@@ -1,11 +1,14 @@
-import {render} from 'ink';
+import {render, Text} from 'ink';
 import { DocumentViewResponse } from "nano";
 import { EitherDocument, EitherPayload } from "../documentControl/DatumDocument";
 import Table from "ink-table";
 import React from 'react';
 
+const CustomSkeleton = ({ children }: React.PropsWithChildren<{}>) => (
+  <React.Fragment />
+);
 
 export function renderView(viewResponse: DocumentViewResponse<any, EitherPayload>): void {
   const data = viewResponse.rows.map((row) => ({key: row.key, value: row.value}));
-  render(<Table data={data} />);
+  render(<Table data={data} skeleton={CustomSkeleton} />);
 }
