ceab7eeccb39f6cd4dbcf585d6b8516fd3df749b
Nate Faber <nathaniel@njfaber.com>
2022-08-15 15:56:24 +0200

    Add categorized balanced view

--- projects/finance/balance.ts
+++ projects/finance/balance.ts
@@ -12,6 +12,7 @@ export type TxDoc = DatumDocument<{
   acc: string;
   amount: number;
   to: string;
+  reverse?: boolean;
   comment?: string | string[];
 }>;
 export type EqDoc = DatumDocument<{
@@ -53,3 +54,33 @@ export const balanceView: DatumView<FinDoc> = {
     collation: "raw",
   },
 };
+
+export const categorizedBalanceView: DatumView<FinDoc> = {
+  name: "categorizedBalance",
+  map: (doc: FinDoc) => {
+    const getAccType = (name: string) => {
+      switch (true) {
+        case /^[A-Z]/.test(name):
+          return "Assets";
+        case /^[a-z]/.test(name):
+          return "expenses";
+        case /^\+|^_/.test(name):
+          return "+income";
+        default:
+          return name[0];
+      }
+    };
+
+    const data = doc.data;
+    if (data.type === "tx") {
+      const amount = data.reverse === true ? data.amount * -1 : data.amount;
+      emit([getAccType(data.acc), data.curr, data.acc, data.occurTime], -amount);
+      emit([getAccType(data.to), data.curr, data.to, data.occurTime], amount);
+    }
+    if (data.type === "xc") {
+      emit([getAccType(data.acc1), data.curr1, data.acc1, data.occurTime], -data.amount1);
+      emit([getAccType(data.acc2), data.curr2, data.acc2, data.occurTime], data.amount2);
+    }
+  },
+  reduce: "_sum",
+}
