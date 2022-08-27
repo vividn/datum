2e4a0cdab880e1148bc6b0908307212d4d84f320
Nate Faber <nathaniel@njfaber.com>
2022-08-27 11:48:30 +0200

    Include last done date in the chore list view

--- projects/task/chores.ts
+++ projects/task/chores.ts
@@ -4,7 +4,7 @@ import { DatumDocument } from "../../src/documentControl/DatumDocument";
 import { isoDate, isoDateOrTime, isoDatetime } from "../../src/time/timeUtils";
 import { DatumView } from "../../src/views/viewDocument";
 
-function emit(doc: unknown, value: unknown) {
+function emit(doc: ChoreMapRow["key"], value: ChoreMapRow["value"]) {
   _emit(doc, value);
 }
 
@@ -17,41 +17,41 @@ type ChoreDoc = TaskDoc &
 
 type ChoreMapRow = {
   key: string // chore name
-  value: { occur: isoDateOrTime, next?: isoDateOrTime }
+  value: { occur: isoDateOrTime, next?: isoDateOrTime, lastDone: isoDateOrTime | "" }
 }
 
 export const choreView: DatumView<ChoreDoc> = {
   name: "chores",
   map: (doc: ChoreDoc) => {
     if (doc.data && doc.data.type === "maintain" && doc.data.occurTime) {
+      const {nextDate, nextTime, occurTime, done} = doc.data;
       let next: isoDateOrTime | null | undefined = undefined;
-      if (doc.data.nextTime) {
-        if (doc.data.nextDate) {
-          const nextTime = new Date(doc.data.nextTime);
-          const nextDate = new Date(doc.data.nextDate);
-          nextTime.setFullYear(
-            nextDate.getFullYear(),
-            nextDate.getMonth(),
-            nextDate.getDate()
+      if (nextTime) {
+        if (nextDate) {
+          const nextTimeParsed = new Date(nextTime);
+          const nextDateParsed = new Date(nextDate);
+          nextTimeParsed.setFullYear(
+            nextDateParsed.getFullYear(),
+            nextDateParsed.getMonth(),
+            nextDateParsed.getDate()
           );
-          next = nextTime.toISOString();
+          next = nextTimeParsed.toISOString();
         } else {
-          next = doc.data.nextTime;
+          next = nextTime;
         }
-      } else if (doc.data.nextDate) {
-        next = doc.data.nextDate;
+      } else if (nextDate) {
+        next = nextDate;
       }
-      emit(doc.data.task, { occur: doc.data.occurTime, next });
+      emit(doc.data.task, { occur: occurTime, next, lastDone: done ? occurTime : "" });
     }
   },
-  reduce: (keysIds, values: ChoreMapRow["value"][], rereduce) => {
+  reduce: (keysIds, values: ChoreMapRow["value"][], _rereduce) => {
     return values.reduce((reduced, currentValue) => {
       const isLatest = currentValue.occur > reduced.occur;
-      if (isLatest) {
-            return currentValue;
-        } else {
-            return reduced;
-        }
+      const latestNext = isLatest ? currentValue.next : reduced.next;
+      const latestOccur = isLatest ? currentValue.occur : reduced.occur;
+      const latestDoneOccur = currentValue.lastDone > reduced.lastDone ? currentValue.lastDone : reduced.lastDone
+      return {occur: latestOccur, lastDone: latestDoneOccur, next: latestNext};
     });
   }
 };
