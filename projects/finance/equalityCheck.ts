fc0e7971d3aafcccfb82039be2280e62a47d0456
Nate Faber <nathaniel@njfaber.com>
2023-01-08 00:04:07 +0100

    Add equalit view to project

---
+++ projects/finance/equalityCheck.ts
@@ -0,0 +1,18 @@
+import { _emit } from "../../src/views/emit";
+import { FinanceDoc } from "./balance";
+import { DatumView } from "../../src/views/viewDocument";
+
+function emit(doc: unknown, value: unknown) {
+  _emit(doc, value);
+}
+
+export const equalityView: DatumView<FinanceDoc> = {
+  name: "equality",
+  map: (doc) => {
+    const data = doc.data;
+    if (data.type === "eq") {
+      emit([data.acc, data.curr, data.occurTime], data.bal);
+    }
+  },
+  reduce: "_count",
+};
