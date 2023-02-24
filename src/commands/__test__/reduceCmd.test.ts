f48230e5033533829be94ea4e7e8c21935fd1102
Nate Faber <nathaniel@njfaber.com>
2023-02-24 15:28:48 +0530

    add some test titles

--- src/commands/__test__/reduceCmd.test.ts
+++ src/commands/__test__/reduceCmd.test.ts
@@ -4,4 +4,6 @@ describe("reduceCmd", () => {
   it.todo("some snapshot on fixture data");
   it.todo("MORE TESTS?");
   it.todo("can count number of documents in _all_docs view");
+  it.todo("does not render if Show is None");
+  it.todo("does render if Show is not None");
 });
