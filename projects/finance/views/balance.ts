f7a642e66cd67e5925f1bb91d8eb1e20fc0ed013
Nate Faber <nathaniel@njfaber.com>
2023-03-08 12:33:16 +0100

    more type fixes

--- projects/finance/views/balance.ts
+++ projects/finance/views/balance.ts
@@ -1,10 +1,7 @@
 import { _emit } from "../../../src/views/emit";
 import { DatumDocument } from "../../../src/documentControl/DatumDocument";
 import { DatumView } from "../../../src/views/DatumView";
-
-function emit(key: unknown, value: unknown) {
-  _emit(key, value);
-}
+import { isoDateOrTime } from "../../../src/time/timeUtils";
 
 export type TxDoc = DatumDocument<{
   type: "tx";
@@ -35,8 +32,20 @@ export type XcDoc = DatumDocument<{
 
 export type FinanceDoc = TxDoc | EqDoc | XcDoc;
 
-export const balanceView: DatumView<FinanceDoc> = {
+type DocType = FinanceDoc;
+type MapKey = [string, string, isoDateOrTime?];
+type MapValue = number;
+type ReduceValues = {
+  default: number;
+};
+
+function emit(key: MapKey, value: MapValue): void {
+  _emit(key, value);
+}
+
+export const balanceView: DatumView<DocType, MapKey, MapValue, ReduceValues> = {
   name: "balance",
+  emit,
   map: (doc: FinanceDoc) => {
     const data = doc.data;
     if (data.type === "tx") {
@@ -49,47 +58,10 @@ export const balanceView: DatumView<FinanceDoc> = {
       emit([data.acc2, data.curr2, data.occurTime], data.amount2);
     }
   },
-  reduce: "_sum",
+  reduce: {
+    default: "_sum",
+  },
   options: {
     collation: "raw",
   },
-};
-
-export const categorizedBalanceView: DatumView<FinanceDoc> = {
-  name: "categorizedBalance",
-  map: (doc: FinanceDoc) => {
-    const getAccType = (name: string) => {
-      switch (true) {
-        case /^[A-Z]/.test(name):
-          return "Assets";
-        case /^[a-z]/.test(name):
-          return "expenses";
-        case /^\+|^_/.test(name):
-          return "+income";
-        default:
-          return name[0];
-      }
-    };
-
-    const data = doc.data;
-    if (data.type === "tx") {
-      const amount = data.reverse === true ? data.amount * -1 : data.amount;
-      emit(
-        [getAccType(data.acc), data.curr, data.acc, data.occurTime],
-        -amount
-      );
-      emit([getAccType(data.to), data.curr, data.to, data.occurTime], amount);
-    }
-    if (data.type === "xc") {
-      emit(
-        [getAccType(data.acc1), data.curr1, data.acc1, data.occurTime],
-        -data.amount1
-      );
-      emit(
-        [getAccType(data.acc2), data.curr2, data.acc2, data.occurTime],
-        data.amount2
-      );
-    }
-  },
-  reduce: "_sum",
 };
--- projects/finance/views/equalityCheck.ts
+++ projects/finance/views/equalityCheck.ts
@@ -2,17 +2,20 @@ import { _emit } from "../../../src/views/emit";
 import { FinanceDoc } from "./balance";
 import { DatumView } from "../../../src/views/DatumView";
 
-function emit(key: unknown, value: unknown) {
+function emit(key: unknown, value: unknown): void {
   _emit(key, value);
 }
 
 export const equalityView: DatumView<FinanceDoc> = {
   name: "equality",
+  emit,
   map: (doc) => {
     const data = doc.data;
     if (data.type === "eq") {
       emit([data.acc, data.curr, data.occurTime], data.bal);
     }
   },
-  reduce: "_count",
+  reduce: {
+    default: "_count",
+  },
 };
