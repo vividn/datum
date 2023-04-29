72080aae9ca80ed0b8add18668ac7ca9437aaee5
Nate Faber <nathaniel@njfaber.com>
2023-04-29 14:43:09 +0200

    Include id in map

--- src/output/renderView.tsx
+++ src/output/renderView.tsx
@@ -13,6 +13,7 @@ export function renderView(
   const data = viewResponse.rows.map((row) => ({
     key: row.key,
     value: row.value,
+    id: row.doc?.meta?.humanId?.slice(0,6) ?? row.id,
   }));
   render(<Table data={data} skeleton={CustomSkeleton} />);
 }
