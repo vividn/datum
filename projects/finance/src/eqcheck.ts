6fb3750b8ed0d4cac07f71136c044fa247da3ffb
Nate Faber <nathaniel@njfaber.com>
2023-05-26 23:21:11 +0200

    projects/finances Separate out transaction view (#476)
    * start factoring out transaction view into separate file
    
    * continue refactoring
    
    * keep refactoring
    
    * fix some array index problems
    
    * fix some bugs
    
    * fix isAllBalanced not updating
    
    * fix bug
    
    * fix short circuit bug
    
    * increase days of context
    
    * fix type problems

--- projects/finance/src/eqcheck.ts
+++ projects/finance/src/eqcheck.ts
@@ -5,24 +5,19 @@ import { balanceView, equalityView } from "../views";
 import { mapCmd } from "../../../src/commands/mapCmd";
 import { reduceCmd } from "../../../src/commands/reduceCmd";
 import { Show } from "../../../src/input/outputArgs";
-import { isoDateOrTime } from "../../../src/time/timeUtils";
-import { TxDoc, XcDoc } from "../views/balance";
-import printf from "printf";
-import chalk from "chalk";
-import { connectDb } from "../../../src/auth/connectDb";
-import * as readline from "node:readline";
-import { stdin } from "node:process";
-import { once } from "node:events";
+import { transactionWatcher, transactionView } from "./transactionView";
+import { DateTime } from "luxon";
 
-const zeroDate = "0000-00-00";
+export const zeroDate = "0000-00-00";
 
-function fix(n: number) {
+export function fix(n: number) {
   return n.toFixed(2);
 }
 
 type EqCheckArgs = BaseArgs & {
   account?: string;
   watch?: boolean;
+  context?: number;
 };
 
 const eqCheckYargs = baseArgs
@@ -30,6 +25,11 @@ const eqCheckYargs = baseArgs
     alias: "a",
     type: "string",
     description: "Account to check",
+  })
+  .option("context", {
+    alias: "C",
+    type: "number",
+    description: "days of context around the equality checks. default: 3",
   })
   .option("watch", {
     alias: "w",
@@ -40,6 +40,7 @@ const eqCheckYargs = baseArgs
 async function main(cliInput: string | string[]) {
   const args = (await eqCheckYargs.parse(cliInput)) as EqCheckArgs;
   args.db ??= "finance";
+  args.context ??= 3;
   const start = args.account ? `,${args.account}` : undefined;
   const allEqualityChecks = (
     await mapCmd({
@@ -49,7 +50,7 @@ async function main(cliInput: string | string[]) {
       show: Show.None,
     })
   ).rows;
-  let lastGoodDate = undefined;
+  let lastGoodEquality = [undefined, undefined, undefined];
   for (const row of allEqualityChecks) {
     const [account, currency, datetime] = row.key;
     const expectedBalance = row.value;
@@ -63,281 +64,40 @@ async function main(cliInput: string | string[]) {
       })
     ).rows[0].value;
     if (fix(expectedBalance) !== fix(actualBalance)) {
+      const lastGoodDate =
+        account === lastGoodEquality[0] && currency === lastGoodEquality[1]
+          ? lastGoodEquality[2]
+          : undefined;
+      const startDate = lastGoodDate
+        ? (DateTime.fromISO(lastGoodDate)
+            .minus({ days: args.context })
+            .toISODate() as string)
+        : undefined;
+      const endDate = DateTime.fromISO(datetime)
+        .plus({ days: args.context })
+        .toISODate() as string;
       if (args.watch) {
-        await balanceWatcher({
+        await transactionWatcher({
           args,
           account,
           currency,
-          failDate: datetime,
-          goodDate: lastGoodDate,
+          endDate,
+          startDate,
         });
       } else {
         await transactionView({
           args,
           account,
           currency,
-          failDate: datetime,
-          goodDate: lastGoodDate,
+          endDate,
+          startDate,
         });
         process.exit(3);
       }
     }
-    lastGoodDate = datetime;
+    lastGoodEquality = row.key;
     console.info(`Balance check passed for ${account} ${currency} ${datetime}`);
   }
-}
-
-type BalanceWatcherInput = {
-  args: BaseArgs;
-  account: string;
-  currency: string;
-  goodDate?: isoDateOrTime;
-  failDate: isoDateOrTime;
-};
-
-async function balanceWatcher({
-  args,
-  account,
-  currency,
-  goodDate = zeroDate,
-  failDate,
-}: BalanceWatcherInput) {
-  let isBalanced = false;
-  async function output() {
-    console.clear();
-    isBalanced = await transactionView({
-      args,
-      account,
-      currency,
-      goodDate,
-      failDate,
-    });
-  }
-
-  const db = connectDb(args);
-  const eventEmitter = db
-    .changes({ since: "now", live: true })
-    .on("change", output)
-    .on("error", (error) => {
-      console.error(error);
-      process.exit(5);
-    });
-  const rl = readline.createInterface({ input: stdin, terminal: true });
-  stdin.setRawMode(true);
-  rl.on("line", output);
-  rl.on("line", () => {
-    if (isBalanced) {
-      rl.close();
-    }
-  });
-  rl.on("close", () => {
-    stdin.setRawMode(false);
-  });
-  rl.on("SIGINT", () => {
-    rl.close();
-    eventEmitter.cancel();
-    process.exit(3);
-  });
-
-  await output();
-  await once(rl, "close");
-  eventEmitter.cancel();
-  return;
-}
-
-async function transactionView({
-  args,
-  account,
-  currency,
-  goodDate = zeroDate,
-  failDate,
-}: BalanceWatcherInput): Promise<boolean> {
-  const goodBalance =
-    ((
-      await reduceCmd({
-        ...args,
-        mapName: balanceView.name,
-        start: `,${account},${currency},${zeroDate}`,
-        end: `,${account},${currency},${goodDate}`,
-        show: Show.None,
-      })
-    ).rows[0]?.value as number) ?? 0;
-  const goodEqualityDoc = (
-    await mapCmd({
-      ...args,
-      mapName: equalityView.name,
-      start: `,${account},${currency},${goodDate}`,
-      show: Show.None,
-      params: { include_docs: true },
-    })
-  ).rows[0];
-  const goodEquality = goodEqualityDoc?.value as number ?? 0;
-  const goodHid = (goodEqualityDoc?.doc?.meta?.humanId as string) ?? "";
-  const failBalance = (
-    await reduceCmd({
-      ...args,
-      mapName: balanceView.name,
-      start: `,${account},${currency},${zeroDate}`,
-      end: `,${account},${currency},"${failDate}"`,
-      show: Show.None,
-    })
-  ).rows[0].value;
-
-  const expectedEquality = (
-    await mapCmd({
-      ...args,
-      mapName: equalityView.name,
-      start: `,${account},${currency},${failDate}`,
-      show: Show.None,
-      params: { include_docs: true },
-    })
-  ).rows[0];
-  const expectedBalance = expectedEquality.value;
-  const expectedEqualityHid =
-    (expectedEquality.doc?.meta?.humanId as string) ?? "";
-  const transactions = (
-    await mapCmd({
-      ...args,
-      mapName: balanceView.name,
-      start: `,${account},${currency},"${goodDate}"`,
-      end: `,${account},${currency},"${failDate}"`,
-      show: Show.None,
-      reverse: true,
-      params: { include_docs: true },
-    })
-  ).rows as PouchDB.Query.Response<TxDoc | XcDoc>["rows"];
-
-  const width = Math.max(Math.min(80, process.stdout.columns), 30);
-  const dateWidth = 10;
-  const hidWidth = 4;
-  const toAccountWidth = 10;
-  const arrowWidth = 1;
-  const amountWidth =
-    Math.ceil(
-      Math.log10(Math.max(1, ...transactions.map((row) => Math.abs(row.value))))
-    ) + 4;
-  const runningTotalWidth =
-    Math.ceil(
-      Math.log10(Math.max(Math.abs(goodBalance), Math.abs(failBalance)))
-    ) + 4;
-  const commentWidth =
-    width -
-    dateWidth -
-    hidWidth -
-    toAccountWidth -
-    arrowWidth -
-    amountWidth -
-    runningTotalWidth -
-    6;
-  const formatString =
-    `%-${dateWidth}.${dateWidth}s ` +
-    `%-${hidWidth}.${hidWidth}s ` +
-    `%-${commentWidth}.${commentWidth}s ` +
-    `%${toAccountWidth}.${toAccountWidth}s ` +
-    `%${arrowWidth}.${arrowWidth}s ` +
-    `%${amountWidth}.2f ` +
-    `%${runningTotalWidth}.2f`;
-
-  console.clear();
-  console.log(chalk.yellow.bold(`${account} ${currency}`));
-  const isBalanced = fix(expectedBalance) === fix(failBalance);
-  console.log(
-    isBalanced
-      ? chalk.greenBright(
-          printf(
-            formatString,
-            failDate,
-            expectedEqualityHid,
-            "EqCheck",
-            "",
-            "",
-            expectedBalance - failBalance,
-            expectedBalance
-          )
-        )
-      : chalk.redBright(
-          printf(
-            formatString,
-            failDate,
-            expectedEqualityHid,
-            "FAIL",
-            "",
-            "",
-            expectedBalance - failBalance,
-            expectedBalance
-          )
-        )
-  );
-  let reverseBalance = failBalance;
-  for (const row of transactions) {
-    const doc = row.doc!;
-    const {
-      data: { comment = "" },
-    } = doc;
-    const hid = doc.meta?.humanId ?? "";
-    const amount = row.value;
-    const toAccount = row.key[3];
-    const arrow = amount > 0 ? "→" : "←";
-    const date = row.key[2];
-    console.log(
-      printf(
-        formatString,
-        date,
-        hid,
-        comment,
-        toAccount,
-        arrow,
-        amount,
-        reverseBalance
-      )
-    );
-    reverseBalance -= amount;
-  }
-
-  console.log(
-    fix(goodEquality) === fix(reverseBalance)
-      ? chalk.greenBright(
-          printf(
-            formatString,
-            goodDate,
-            goodHid,
-            "EqCheck",
-            "",
-            "",
-            0,
-            goodBalance
-          ).replace(/0\.00/, "    ")
-        )
-      : chalk.redBright(
-          printf(
-            formatString,
-            goodDate,
-            goodHid,
-            "FAIL",
-            "",
-            "",
-            goodBalance - goodEquality,
-            reverseBalance
-          )
-        )
-  );
-
-  if (fix(reverseBalance) !== fix(goodBalance)) {
-    console.warn(
-      chalk.red.bold(
-        `Running total does not align with data. Something went wrong. Expected: ${fix(
-          goodBalance
-        )}, got ${fix(reverseBalance)} (${fix(reverseBalance - goodBalance)})`
-      )
-    );
-  }
-
-  if (fix(goodBalance) !== fix(goodEquality)) {
-    console.warn(
-      chalk.red.bold(`Initial balance has changed. May want to rerun.`)
-    );
-  }
-  return isBalanced;
 }
 
 if (require.main === module) {
---
+++ projects/finance/src/transactionView.ts
@@ -0,0 +1,256 @@
+import { connectDb } from "../../../src/auth/connectDb";
+import readline from "node:readline";
+import { stdin } from "node:process";
+import { once } from "node:events";
+import { BaseArgs } from "../../../src/input/baseArgs";
+import { isoDateOrTime } from "../../../src/time/timeUtils";
+import { reduceCmd } from "../../../src/commands/reduceCmd";
+import { balanceView, equalityView } from "../views";
+import { Show } from "../../../src/input/outputArgs";
+import { mapCmd } from "../../../src/commands/mapCmd";
+import { EqDoc, TxDoc, XcDoc } from "../views/balance";
+import chalk from "chalk";
+import printf from "printf";
+import { fix, zeroDate } from "./eqcheck";
+
+type TransactionViewInput = {
+  args: BaseArgs;
+  account: string;
+  currency: string;
+  endDate: isoDateOrTime;
+  startDate?: isoDateOrTime;
+};
+
+export async function transactionWatcher({
+  args,
+  account,
+  currency,
+  startDate = zeroDate,
+  endDate,
+}: TransactionViewInput): Promise<void> {
+  let isBalanced = false;
+
+  async function output() {
+    console.clear();
+    isBalanced = await transactionView({
+      args,
+      account,
+      currency,
+      startDate: startDate,
+      endDate: endDate,
+    });
+  }
+
+  const db = connectDb(args);
+  const eventEmitter = db
+    .changes({ since: "now", live: true })
+    .on("change", output)
+    .on("error", (error) => {
+      console.error(error);
+      process.exit(5);
+    });
+  const rl = readline.createInterface({ input: stdin, terminal: true });
+  stdin.setRawMode(true);
+  rl.on("line", output);
+  rl.on("line", () => {
+    if (isBalanced) {
+      rl.close();
+    }
+  });
+  rl.on("close", () => {
+    stdin.setRawMode(false);
+  });
+  rl.on("SIGINT", () => {
+    rl.close();
+    eventEmitter.cancel();
+    process.exit(3);
+  });
+
+  await output();
+  await once(rl, "close");
+  eventEmitter.cancel();
+  return;
+}
+
+export async function transactionView({
+  args,
+  account,
+  currency,
+  endDate,
+  startDate = zeroDate,
+}: TransactionViewInput): Promise<boolean> {
+  const startBalance =
+    ((
+      await reduceCmd({
+        ...args,
+        mapName: balanceView.name,
+        start: `,${account},${currency},${zeroDate}`,
+        end: `,${account},${currency},${startDate}`,
+        show: Show.None,
+      })
+    ).rows[0]?.value as number) ?? 0;
+  const endBalance = (
+    await reduceCmd({
+      ...args,
+      mapName: balanceView.name,
+      start: `,${account},${currency},${zeroDate}`,
+      end: `,${account},${currency},"${endDate}"`,
+      show: Show.None,
+    })
+  ).rows[0].value;
+  const transactions = (
+    await mapCmd({
+      ...args,
+      mapName: balanceView.name,
+      start: `,${account},${currency},"${startDate}"`,
+      end: `,${account},${currency},"${endDate}"`,
+      show: Show.None,
+      reverse: true,
+      params: { include_docs: true },
+    })
+  ).rows as PouchDB.Query.Response<TxDoc | XcDoc>["rows"];
+  const equalities = (
+    await mapCmd({
+      ...args,
+      mapName: equalityView.name,
+      start: `,${account},${currency},"${startDate}"`,
+      end: `,${account},${currency},"${endDate}"`,
+      show: Show.None,
+      reverse: true,
+      params: { include_docs: true },
+    })
+  ).rows as PouchDB.Query.Response<EqDoc>["rows"];
+
+  const width = Math.max(Math.min(80, process.stdout.columns), 30);
+  const dateWidth = 10;
+  const hidWidth = 4;
+  const toAccountWidth = 10;
+  const arrowWidth = 1;
+  const amountWidth =
+    Math.ceil(
+      Math.log10(Math.max(1, ...transactions.map((row) => Math.abs(row.value))))
+    ) + 4;
+  const runningTotalWidth =
+    Math.ceil(
+      Math.log10(
+        Math.max(
+          Math.abs(startBalance),
+          Math.abs(endBalance),
+          ...equalities.map((row) => Math.abs(row.value))
+        )
+      )
+    ) + 4;
+  const commentWidth =
+    width -
+    dateWidth -
+    hidWidth -
+    toAccountWidth -
+    arrowWidth -
+    amountWidth -
+    runningTotalWidth -
+    6;
+  const format =
+    `%-${dateWidth}.${dateWidth}s ` +
+    `%-${hidWidth}.${hidWidth}s ` +
+    `%-${commentWidth}.${commentWidth}s ` +
+    `%${toAccountWidth}.${toAccountWidth}s ` +
+    `%${arrowWidth}.${arrowWidth}s ` +
+    `%${amountWidth}.2f ` +
+    `%${runningTotalWidth}.2f`;
+
+  console.log(chalk.yellow.bold(`${account} ${currency}`));
+  let reverseBalance = endBalance;
+  let isAllBalanced = true;
+
+  function displayEquality(
+    equality: PouchDB.Query.Response<EqDoc>["rows"][0],
+    currentBalance: number
+  ): boolean {
+    const date = equality.key[2];
+    const hid = equality.doc?.meta?.humanId ?? "";
+    const eqBalance = equality.value;
+    const amount = eqBalance - currentBalance;
+    const isBalanced = fix(eqBalance) === fix(currentBalance);
+    console.log(
+      isBalanced
+        ? chalk.greenBright(
+            printf(format, date, hid, "EqCheck", "", "", 0, eqBalance).replace(
+              /0\.00/,
+              "    "
+            )
+          )
+        : chalk.redBright(
+            printf(format, date, hid, "FAIL", "", "", amount, eqBalance)
+          )
+    );
+    return isBalanced;
+  }
+
+  function displayTransaction(
+    transaction: PouchDB.Query.Response<TxDoc | XcDoc>["rows"][0],
+    currentBalance: number
+  ): number {
+    const doc = transaction.doc!;
+    const {
+      data: { comment = "" },
+    } = doc;
+    const hid = doc.meta?.humanId ?? "";
+    const amount = transaction.value;
+    const toAccount = transaction.key[3];
+    const arrow = amount > 0 ? "→" : "←";
+    const date = transaction.key[2];
+    console.log(
+      printf(
+        format,
+        date,
+        hid,
+        comment,
+        toAccount,
+        arrow,
+        amount,
+        currentBalance
+      )
+    );
+    return amount;
+  }
+
+  while (transactions.length || equalities.length) {
+    const transactionDate = transactions[0]?.key[2] ?? zeroDate;
+    const equalityDate = equalities[0]?.key[2] ?? zeroDate;
+    if (
+      !transactions.length ||
+      (equalityDate >= transactionDate && equalities.length)
+    ) {
+      const balanced = displayEquality(equalities.shift()!, reverseBalance);
+      isAllBalanced &&= balanced;
+    } else {
+      reverseBalance -= displayTransaction(
+        transactions.shift()!,
+        reverseBalance
+      );
+    }
+  }
+  if (fix(reverseBalance) === "-0.00") {
+    reverseBalance = 0;
+  }
+  if (startDate === zeroDate) {
+    displayEquality(
+      {
+        key: [account, currency, zeroDate],
+        value: 0,
+        id: "",
+        doc: undefined,
+      },
+      reverseBalance
+    );
+  }
+
+  if (fix(reverseBalance) !== fix(startBalance)) {
+    throw new Error(
+      `Running total does not align with data. Something went wrong. Expected: ${fix(
+        startBalance
+      )}, got ${fix(reverseBalance)} (${fix(reverseBalance - startBalance)})`
+    );
+  }
+  return isAllBalanced;
+}
