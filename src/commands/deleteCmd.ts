73eee726584e522805ef4a7fecede87e771f5f6b
Nate Faber <nathaniel@njfaber.com>
2023-06-18 09:22:37 +0200

    get deleteCmd working with multiple ids

--- src/commands/deleteCmd.ts
+++ src/commands/deleteCmd.ts
@@ -14,9 +14,13 @@ export function builder(yargs: Argv): Argv {
   return quickIdArg(yargs);
 }
 
-export async function deleteCmd(args: DeleteCmdArgs): Promise<DeletedDocument> {
+export async function deleteCmd(
+  args: DeleteCmdArgs
+): Promise<DeletedDocument[]> {
   const db = connectDb(args);
-  const id = await quickIds(db, args.quickId);
+  const ids = await quickIds(db, args.quickId);
 
-  return await deleteDoc({ id, db, outputArgs: args });
+  return await Promise.all(
+    ids.map((id) => deleteDoc({ id, db, outputArgs: args }))
+  );
 }
