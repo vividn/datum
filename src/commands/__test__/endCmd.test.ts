dd8ab34e7282f7e7940e1426403e6d21c9898b3a
Nate Faber <nathaniel@njfaber.com>
2023-09-02 12:30:54 +0200

    add tests for startCmd and endCmd

--- src/commands/__test__/endCmd.test.ts
+++ src/commands/__test__/endCmd.test.ts
@@ -1,4 +1,29 @@
+import { setNow, testDbLifecycle } from "../../test-utils";
+import { endCmd } from "../endCmd";
+import { DateTime } from "luxon";
+import * as switchCmdModle from "../switchCmd";
+import { setupCmd } from "../setupCmd";
+
 describe("endCmd", () => {
-  it.todo("creates an occur document with state: false");
-  it.todo("calls switchCmd with state: false");
+  const dbName = "end_cmd_test";
+  const db = testDbLifecycle(dbName);
+  beforeEach(async () => {
+    await setupCmd({});
+    setNow("2023-09-02,11:45");
+  });
+
+  it("creates an occur document with state: false", async () => {
+    const doc = await endCmd({ field: "dance" });
+    expect(doc.data).toMatchObject({
+      field: "dance",
+      state: false,
+      occurTime: DateTime.now().toUTC().toISO(),
+    });
+  });
+
+  it("calls switchCmd with state: false", async () => {
+    const switchCmdSpy = jest.spyOn(switchCmdModle, "switchCmd");
+    await endCmd({ field: "mop" });
+    expect(switchCmdSpy).toBeCalledWith({ field: "mop", state: false });
+  });
 });
--- src/commands/__test__/startCmd.test.ts
+++ src/commands/__test__/startCmd.test.ts
@@ -1,4 +1,30 @@
+import { setNow, testDbLifecycle } from "../../test-utils";
+import { DateTime } from "luxon";
+import * as switchCmdModle from "../switchCmd";
+import { setupCmd } from "../setupCmd";
+import { startCmd } from "../startCmd";
+
 describe("startCmd", () => {
-  it.todo("creates an occur document with state: true");
-  it.todo("calls switchCmd with state: true");
+  const dbName = "start_cmd_test";
+  const db = testDbLifecycle(dbName);
+  beforeEach(async () => {
+    await setupCmd({});
+    setNow("2023-09-02,12:30");
+  });
+
+  it("creates an occur document with state: true", async () => {
+    const doc = await startCmd({ field: "dance" });
+    expect(doc.data).toMatchObject({
+      field: "dance",
+      state: true,
+      occurTime: DateTime.now().toUTC().toISO(),
+    });
+  });
+
+  it("calls switchCmd with state: true", async () => {
+    const switchCmdSpy = jest.spyOn(switchCmdModle, "switchCmd");
+    await startCmd({ field: "mop" });
+    expect(switchCmdSpy).toBeCalledWith({ field: "mop", state: true });
+  });
 });
+
