34c45eecb0bb0d20bf783d01ce6c5d90931e4219
Nate Faber <nathaniel@njfaber.com>
2023-09-03 10:07:02 +0200

    Lint code

--- src/state/getActiveState.ts
+++ src/state/getActiveState.ts
@@ -1,5 +1,8 @@
 import { DateTime } from "luxon";
-import { activeStateView, DatumState } from "../views/datumViews/activeStateView";
+import {
+  activeStateView,
+  DatumState,
+} from "../views/datumViews/activeStateView";
 import { now } from "../time/timeUtils";
 import { DatumViewMissingError, isCouchDbError } from "../errors";
 import { parseTimeStr } from "../time/parseTimeStr";
