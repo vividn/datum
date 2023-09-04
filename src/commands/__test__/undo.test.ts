75a89380ad16d787613679a82d0e8adc37a166b8
Nate Faber <nathaniel@njfaber.com>
2023-09-04 12:54:44 +0200

    start setting up undo command

---
+++ src/commands/__test__/undo.test.ts
@@ -0,0 +1,9 @@
+describe("undoCmd", () => {
+  it.todo("can undo an add command");
+  it.todo("can undo an update command");
+  it.todo("can undo a document based off of specific parameters, even if a different document of the same field was modified more recently");
+  it.todo("can undo a delete command");
+  it.todo("can undo a change where the id was changed");
+  it.todo("can undo based on a quickId");
+  it.todo("prioritizes a field over a quickId");
+})
