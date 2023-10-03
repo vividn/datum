784bc640a92e574f981cd175c2f90ea55e22ac8b
Nate Faber <nathaniel@njfaber.com>
2023-10-03 13:57:10 +0200

    add lastDocRef for deleteCmd

--- src/documentControl/__test__/lastDocs.test.ts
+++ src/documentControl/__test__/lastDocs.test.ts
@@ -1,6 +1,7 @@
 import { addCmd } from "../../commands/addCmd";
 import { getLastDocs } from "../lastDocs";
 import { testDbLifecycle } from "../../test-utils";
+import { deleteCmd } from "../../commands/deleteCmd";
 
 describe("lastDocs", () => {
   const dbName = "last_docs_test";
@@ -13,5 +14,16 @@ describe("lastDocs", () => {
     });
     const lastDocs = await getLastDocs(db);
     expect(lastDocs.ids).toEqual([doc._id]);
+  });
+
+  test("deleteCmd updates the local last doc reference", async () => {
+    const id = "last-doc-deleted";
+    await db.put({ _id: id, foo: "bar" });
+    await db.put({ _id: "some-other-doc", foo: "baz" });
+    await expect(getLastDocs(db)).rejects.toThrowError();
+
+    await deleteCmd({ quickId: id });
+    const lastDocsRef = await getLastDocs(db);
+    expect(lastDocsRef.ids).toEqual([id]);
   });
 });
