52e25503b0d6b643887ea0365587beca1bffd204
Nate Faber <nathaniel@njfaber.com>
2023-10-10 16:16:08 +0200

    write more tests for lastDocs

--- src/documentControl/__test__/lastDocs.test.ts
+++ src/documentControl/__test__/lastDocs.test.ts
@@ -6,6 +6,9 @@ import * as editInTerminal from "../../utils/editInTerminal";
 import { GenericObject } from "../../GenericObject";
 import { editCmd } from "../../commands/editCmd";
 import { setupCmd } from "../../commands/setupCmd";
+import { getCmd } from "../../commands/getCmd";
+import { endCmd } from "../../commands/endCmd";
+import { grepCmd } from "../../commands/grepCmd";
 
 describe("lastDocs", () => {
   const dbName = "last_docs_test";
@@ -44,5 +47,34 @@ describe("lastDocs", () => {
     await editCmd({ quickId: id });
     const lastDocsRef = await getLastDocs(db);
     expect(lastDocsRef.ids).toEqual([id]);
+  });
+
+  test("endCmd updates lastDocRef", async () => {
+    const { _id } = await endCmd({ field: "field" });
+    const lastDocsRef = await getLastDocs(db);
+    expect(lastDocsRef.ids).toEqual([_id]);
+  });
+
+  test("getCmd updates lastDocRef", async () => {
+    const id = "last-doc-gotten-1";
+    await db.put({ _id: id, foo: "bar" });
+    await getCmd({ quickId: id });
+    const lastDocsRef = await getLastDocs(db);
+    expect(lastDocsRef.ids).toEqual([id]);
+  });
+
+  test("grepCmd updates lastDocRef", async () => {
+    await db.bulkDocs([
+      { _id: "a", has: "match" },
+      { _id: "b", data: { also: "has match" } },
+      { _id: "c", data: { does: "not" } },
+      { _id: "d", data: {} },
+      { _id: "e", match: "has" },
+      { _id: "f", data: { match: "also grepped" } },
+    ]);
+    const docs = await grepCmd({ patterns: ["match"] });
+    expect(docs.map((doc) => doc._id)).toEqual(["a", "b", "e", "f"]);
+    const lastDocRef = await getLastDocs(db);
+    expect(lastDocRef.ids).toEqual(["a", "b", "e", "f"]);
   });
 });
