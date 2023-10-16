7caa813b772b87b7baa09fe1c8dae2f4190d6bdd
Nate Faber <nathaniel@njfaber.com>
2023-10-16 17:29:27 +0200

    add more to sample day

--- src/commands/__test__/tailCmd.test.ts
+++ src/commands/__test__/tailCmd.test.ts
@@ -6,15 +6,23 @@ import { switchCmd } from "../switchCmd";
 const today = "2023-10-16";
 async function generateSampleDay(date: string) {
   setNow(`8:30 ${date}`);
-  await endCmd({field: "sleep"});
+  await endCmd({ field: "sleep" });
   setNow("+5");
-  await startCmd({field: "sleep", yesterday: 1, time: "23:20"});
+  await startCmd({ field: "sleep", yesterday: 1, time: "23:20" });
   setNow("8:40");
-  await switchCmd({field: "project", state: "german"});
-  await switchCmd({field: "text", state: "fiction_book"});
+  await switchCmd({ field: "project", state: "german" });
+  await switchCmd({ field: "text", state: "fiction_book" });
   setNow("9:10");
-  await endCmd({field: "project"});
-  await endCmd({field: "text"});
+  await endCmd({ field: "project" });
+  await endCmd({ field: "text" });
+  setNow("9:30");
+  await switchCmd({ field: "environment", state: "outside" });
+  await startCmd({ field: "stretch" });
+  setNow("+7");
+  await endCmd({ field: "stretch" });
+  await startCmd({ field: "run" });
+  setNow("+30");
+  await endCmd({ field: "run", data: ["distance=5.4"] });
 }
 
 describe("tailCmd", () => {
@@ -23,6 +31,7 @@ describe("tailCmd", () => {
   const _db = testDbLifecycle(dbName);
 
   it.todo("displays by default the last 10 occurrences in the database");
+  it.todo("will display all if there are less than 10 occurences in db");
   it.todo("can display the last n occurrences");
   it.todo("displays last occurrences of a specific field");
   it.todo("defaults to a hybrid display of occurTime and createTime");
@@ -30,8 +39,12 @@ describe("tailCmd", () => {
   it.todo("can display modifyTime tail");
   it.todo("can display createTime tail");
   it.todo("can display a tail from a certain moment in time");
-  it.todo("displays future entries if no specific time is given or --no-timestamp is given");
-  it.todo("does not display future entries if now is given specifically as the time");
+  it.todo(
+    "displays future entries if no specific time is given or --no-timestamp is given"
+  );
+  it.todo(
+    "does not display future entries if now is given specifically as the time"
+  );
   it.todo("displays all occurrences on a day if date is given without time");
   it.todo(
     "displays only n latest occurrences on a day if date and -n is given"
