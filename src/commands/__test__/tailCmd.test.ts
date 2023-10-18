06a40001698040ff91997a280733c97757d93b2c
Nate Faber <nathaniel@njfaber.com>
2023-10-18 12:51:52 +0200

    write some more tests

--- src/commands/__test__/tailCmd.test.ts
+++ src/commands/__test__/tailCmd.test.ts
@@ -12,8 +12,13 @@ import { occurCmd } from "../occurCmd";
 import { tailCmd } from "../tailCmd";
 import { Show } from "../../input/outputArgs";
 import { setupCmd } from "../setupCmd";
+import { addCmd } from "../addCmd";
+import { updateCmd } from "../updateCmd";
 
+const yesterday = "2023-10-15";
 const today = "2023-10-16";
+const tomorrow = "2023-10-17";
+
 async function generateSampleMorning(date: string) {
   pushNow(`8:30 ${date}`);
   await endCmd({ field: "sleep" });
@@ -55,6 +60,7 @@ describe("tailCmd", () => {
 
   beforeEach(async () => {
     await setupCmd({});
+    setNow(`20:00 ${today}`);
   });
 
   it("displays by default the last 10 occurrences in the database", async () => {
@@ -101,12 +107,57 @@ describe("tailCmd", () => {
     expect(docs.length).toBe(3);
   });
 
-  it.todo("displays last occurrences of a specific field");
-  it.todo("defaults to a hybrid display of occurTime and createTime");
+  it("displays last occurrences of a specific field", async () => {
+    await generateSampleMorning(today);
+    const docs = await tailCmd({ field: "stretch" });
+    expect(docs.length).toBe(4);
+    await generateSampleMorning(yesterday);
+    const docs2 = await tailCmd({ field: "stretch" });
+    expect(docs2.length).toBe(8);
+    expect(docs2.map((doc) => doc.data.field)).toEqual(
+      Array(8).fill("stretch")
+    );
+  });
+
+  it("defaults to a hybrid display of occurTime and createTime", async () => {
+    setNow("19:00");
+    const { _id: occur1Id } = await occurCmd({ field: "alcohol" });
+    setNow("20:00");
+    const { _id: addId } = await addCmd({
+      field: "person",
+      baseData: { name: "john doe", age: 35 },
+      idPart: "%name",
+    });
+    setNow("21:00");
+    const { _id: occur2Id } = await startCmd({
+      field: "socialize",
+      time: "19:30",
+    });
+    const { _id: occur3Id } = await endCmd({ field: "socialize" });
+
+    // modification does not change the placement in tail, since hybrid is based off of creation when there is no occurence
+    setNow("22:00");
+    await updateCmd({ quickId: addId, data: ["age=36"] });
+
+    const docs = await tailCmd({ show: Show.Standard });
+    expect(docs.length).toBe(4);
+    expect(docs.map((doc) => doc._id)).toEqual([
+      occur1Id,
+      occur2Id,
+      addId,
+      occur3Id,
+    ]);
+    expect(mockedLog.mock.calls).toMatchSnapshot();
+  });
+
   it.todo("can just display occurTime tail");
+
   it.todo("can display modifyTime tail");
+
   it.todo("can display createTime tail");
+
   it.todo("can display a tail from a certain moment in time");
+
   it.todo(
     "displays future entries if no specific time is given or --no-timestamp is given"
   );
