cc4d2ca7dce34586032448cf7af4bf7fbbe91727
Nate Faber <nathaniel@njfaber.com>
2023-11-02 16:52:29 +0000

    Lint code

--- src/views/datumViews/__test__/timingView.test.ts
+++ src/views/datumViews/__test__/timingView.test.ts
@@ -3,5 +3,7 @@ describe("timingView", () => {
     "emits occur, modify, and createTime for field and for null (all fields)"
   );
   it.todo("emits occur, modify and createTime if there is no field");
-  it.todo("does not emit occurTime if there is no occurTime, but emits other times");
+  it.todo(
+    "does not emit occurTime if there is no occurTime, but emits other times"
+  );
 });
