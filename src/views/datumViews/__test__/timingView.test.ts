4f946d139aa20591dbd7d9aab1886140713de846
Nate Faber <nathaniel@njfaber.com>
2023-11-02 16:52:24 +0000

    remove untrue todo test

--- src/views/datumViews/__test__/timingView.test.ts
+++ src/views/datumViews/__test__/timingView.test.ts
@@ -3,8 +3,5 @@ describe("timingView", () => {
     "emits occur, modify, and createTime for field and for null (all fields)"
   );
   it.todo("emits occur, modify and createTime if there is no field");
-  it.todo(
-    "does not emit occurTime, if occurTime is a just a date, but still emits modify and createTime"
-  );
-  it.todo("does not emit occurTime if there is no occurTime");
+  it.todo("does not emit occurTime if there is no occurTime, but emits other times");
 });
