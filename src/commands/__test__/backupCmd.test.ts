9f5e940eeb29d0ad7e89716410dbf52cdbe6d703
Nate Faber <nathaniel@njfaber.com>
2023-11-17 16:05:14 +0100

    write some clobber protection

--- src/commands/__test__/backupCmd.test.ts
+++ src/commands/__test__/backupCmd.test.ts
@@ -3,23 +3,75 @@ import { testDbLifecycle } from "../../test-utils";
 import * as fs from "fs";
 import path from "path";
 import * as os from "os";
+import { addCmd } from "../addCmd";
+import { occurCmd } from "../occurCmd";
+import { EitherDocument } from "../../documentControl/DatumDocument";
 
 describe("backupCmd", () => {
   const dbName = "backup_cmd_test";
   const db = testDbLifecycle(dbName);
 
-  it("creates a compressed backup file that can contains all the documents", async () => {
-    await db.put({ _id: "1", name: "John" });
-    await db.put({ _id: "2", name: "Jane" });
-    // create a tempoarary file path
-    const backupFilePath = path.join(os.tmpdir(), "backup.json");
-    fs.unlinkSync(backupFilePath);
+  let backupFilePath: string;
+  let dbDocs: EitherDocument[] = [];
+  beforeEach(async () => {
+    backupFilePath = path.join(os.tmpdir(), "backup.json.br");
+    if (fs.existsSync(backupFilePath)) {
+      fs.unlinkSync(backupFilePath);
+    }
+    dbDocs.push(
+      await addCmd({
+        field: "added_field",
+        baseData: { some: "data", another: "field" },
+      })
+    );
+    dbDocs.push(
+      await occurCmd({
+        field: "added_field",
+        baseData: { some: "data", another: "field" },
+      })
+    );
+  });
+
+  afterEach(() => {
+    if (fs.existsSync(backupFilePath)) {
+      fs.unlinkSync(backupFilePath);
+    }
+    dbDocs = [];
+  });
+
+  it("creates a backup file that can contains all the documents", async () => {
+    expect(fs.existsSync(backupFilePath)).toBe(false);
     await backupCmd({
       filename: backupFilePath,
     });
-    // expect file to exist
     expect(fs.existsSync(backupFilePath)).toBe(true);
-    // expect file after uncompressing to contain all documents
-    const backupFile = fs.readFileSync(backupFilePath, "utf-8");
+    const loadedBackup = JSON.parse(fs.readFileSync(backupFilePath).toString());
+    const docs = loadedBackup.docs;
+    expect(docs.length).toBe(dbDocs.length);
+    dbDocs.forEach((doc) => {
+      expect(docs).toContainEqual(doc);
+    });
   });
-})
+
+  it("does not clobber the backup file by default", async () => {
+    await backupCmd({
+      filename: backupFilePath,
+    });
+    expect(fs.existsSync(backupFilePath)).toBe(true);
+    await expect(backupCmd({ filename: backupFilePath })).rejects.toThrow(
+      "File exists"
+    );
+  });
+
+  it("allows clobber if --clobber is specified", async () => {
+    await backupCmd({
+      filename: backupFilePath,
+    });
+    expect(fs.existsSync(backupFilePath)).toBe(true);
+    await backupCmd({
+      filename: backupFilePath,
+      clobber: true,
+    });
+    expect(fs.existsSync(backupFilePath)).toBe(false);
+  });
+});
--- src/commands/backupCmd.ts
+++ src/commands/backupCmd.ts
@@ -2,29 +2,41 @@ import { Argv } from "yargs";
 import { MainDatumArgs } from "../input/mainYargs";
 import { connectDb } from "../auth/connectDb";
 import { createWriteStream } from "fs";
-import { brotliCompressSync } from "zlib";
+import * as fs from "fs";
 
 export const command = "backup <filename>";
 export const desc = "Backup db, outputs to stdout";
 
 export type BackupCmdArgs = MainDatumArgs & {
   filename: string;
+  clobber?: boolean;
 };
 
 export function builder(yargs: Argv): Argv {
-  return yargs.positional("filename", {
-    type: "string",
-    args: 1,
-  });
+  return yargs
+    .positional("filename", {
+      type: "string",
+      args: 1,
+    })
+    .options({
+      clobber: {
+        type: "boolean",
+        default: false,
+      },
+    });
 }
 
 export async function backupCmd(args: BackupCmdArgs): Promise<void> {
+  if(fs.existsSync(args.filename)) {
+    throw new Error("File exists");
+  }
   const writeStream = createWriteStream(args.filename);
   const db = connectDb(args);
   const allDocs = (await db.allDocs({ include_docs: true })).rows.map(
     ({ doc }) => doc
   );
-  const buffer = Buffer.from(JSON.stringify(allDocs, null, 0));
-  const compressed = await brotliCompressSync(buffer);
-  await writeStream.write(compressed);
+  // TODO: Also backup and restore attachments (even though using attachments is considered not best practice)
+  const buffer = Buffer.from(JSON.stringify({ docs: allDocs }, null, 0));
+  writeStream.write(buffer);
+  writeStream.end();
 }
