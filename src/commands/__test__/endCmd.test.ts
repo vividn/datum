3a5c218efb48efb42cb39b20a2194923f553e384
Nate Faber <nathaniel@njfaber.com>
2023-12-11 14:05:24 +0100

    add test for endCmd

--- src/commands/__test__/endCmd.test.ts
+++ src/commands/__test__/endCmd.test.ts
@@ -3,10 +3,13 @@ import { endCmd } from "../endCmd";
 import { DateTime } from "luxon";
 import { setupCmd } from "../setupCmd";
 import { switchCmd } from "../switchCmd";
+import { startCmd } from "../startCmd";
+import { getActiveState } from "../../state/getActiveState";
+import { parseTimeStr } from "../../time/parseTimeStr";
 
 describe("endCmd", () => {
   const dbName = "end_cmd_test";
-  testDbLifecycle(dbName);
+  const db = testDbLifecycle(dbName);
   beforeEach(async () => {
     await setupCmd({});
     setNow("2023-09-02,11:45");
@@ -35,5 +38,27 @@ describe("endCmd", () => {
         utc: DateTime.now().toUTC().toISO(),
       },
     });
+  });
+
+  it("can create a hole of time when duration is present", async () => {
+    // Create an initial start of dancing in which to put a hole afterward
+    await startCmd({ field: "dance", time: "-20" });
+    const doc = await endCmd({ field: "dance", duration: "5" });
+    expect(doc.data).toMatchObject({
+      field: "dance",
+      state: false,
+      dur: "PT5M",
+      occurTime: {
+        utc: DateTime.now().toUTC().toISO(),
+      },
+    });
+    const currentState = await getActiveState(db, "dance");
+    expect(currentState).toBe(false);
+    const intermediateState = await getActiveState(
+      db,
+      "dance",
+      parseTimeStr({ timeStr: "-2.5" }),
+    );
+    expect(intermediateState).toBe(false);
   });
 });
