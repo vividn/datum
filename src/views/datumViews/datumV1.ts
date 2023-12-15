ef5d331ed77892fdd5c76d56254b2074570df2b7
Nate Faber <nathaniel@njfaber.com>
2023-12-15 11:22:19 +0100

    revert datumV1 view

--- src/views/datumViews/datumV1.ts
+++ src/views/datumViews/datumV1.ts
@@ -71,28 +71,24 @@ export const datumV1View: DatumView<DocType, MapKey, MapValue, number> = {
           durObj.seconds / 60)
       : "";
     const state = data.state;
-    if (state === null) {
-      outputArray.push("[end]");
-    } else if (state === undefined) {
-      outputArray.push(String(minutes));
-    } else if (Array.isArray(state)) {
-      outputArray.push(String(minutes));
-      outputArray.push(state.map((stateObj) => String(stateObj.id)).join(","));
-    } else if (state.id === false) {
+
+    if (state === false) {
       if (minutes === "") {
         outputArray.push("end");
       } else {
         outputArray.push(String(-1 * minutes));
       }
-    } else if (state.id === true) {
+    } else if (state === true) {
       if (minutes === "") {
         outputArray.push("start");
       } else {
         outputArray.push(String(minutes));
       }
+    } else if (state === undefined) {
+      outputArray.push(String(minutes));
     } else {
       outputArray.push(String(minutes));
-      outputArray.push(String(state.id));
+      outputArray.push(String(state));
     }
 
     emit(key, outputArray);
