0b895806a5b5b7d104128729481fcb163ef41966
Nate Faber <nathaniel@njfaber.com>
2023-12-21 12:05:18 +0100

    write a bunch of tests for changeDatumCommand

--- src/utils/__test__/changeDatumCommand.test.ts
+++ src/utils/__test__/changeDatumCommand.test.ts
@@ -1,27 +1,12 @@
 import { DatumData } from "../../documentControl/DatumDocument";
 import { changeDatumCommand } from "../changeDatumCommand";
-import { DataArgs } from "../../input/dataArgs";
 import { toDatumTime } from "../../time/timeUtils";
+import { setNow } from "../../__test__/test-utils";
+import { DataArgs } from "../../input/dataArgs";
 
+const nowStr = "2023-12-20T17:00:00.000Z";
 const occurTime = toDatumTime("2023-12-20T17:00:00.000Z");
-const noOccurData: DatumData = {
-  existing: "data"
-};
-const occurData: DatumData = {
-  existing: "data",
-  occurTime
-};
-const startData: DatumData = {
-  existing: "data",
-  state: true,
-  occurTime
-};
-const endData: DatumData = {
-  existing: "data",
-  state: false,
-  occurTime,
-};
-
+setNow("2023-12-20T17:00:00.000Z");
 describe("changeDatumCommand", () => {
   let datumData: DatumData;
   describe("start", () => {
@@ -30,7 +15,7 @@ describe("changeDatumCommand", () => {
         existing: "data",
       };
       changeDatumCommand(datumData, "start");
-      expect(datumData).toEqual({
+      expect(datumData).toMatchObject({
         existing: "data",
         state: {
           id: true,
@@ -38,33 +23,272 @@ describe("changeDatumCommand", () => {
       });
     });
 
-    it.todo("keeps a preexisting state");
-    it.todo("adds occurTime=now if it doesn't exist");
-    it.todo("removes dur=null from the data");
-    it.todo("adds a dur key to the beginning of the optional keys array");
+    it("keeps a preexisting state", () => {
+      datumData = {
+        state: { id: "outside", weather: "rainy" },
+        field: "environment",
+      };
+      changeDatumCommand(datumData, "start");
+      expect(datumData).toMatchObject({
+        state: { id: "outside", weather: "rainy" },
+        field: "environment",
+      });
+    });
+
+    it("keeps a prexisting state but changes state.id to true if state.id is false", () => {
+      datumData = {
+        field: "project",
+        state: {
+          id: false,
+          countdown: 10,
+        },
+      };
+      changeDatumCommand(datumData, "start");
+      expect(datumData).toMatchObject({
+        field: "project",
+        state: {
+          id: true,
+          countdown: 10,
+        },
+      });
+    });
+
+    it("adds occurTime=now if it doesn't exist", () => {
+      datumData = {
+        existing: "data",
+      };
+      changeDatumCommand(datumData, "start");
+      expect(datumData).toMatchObject({
+        occurTime,
+      });
+    });
+
+    it("keeps an existing occurTime", () => {
+      const explicitOccur = toDatumTime("2023-12-21T11:45:00.000");
+      datumData = {
+        occurTime: explicitOccur,
+      };
+      changeDatumCommand(datumData, "start");
+      expect(datumData).toMatchObject({
+        occurTime: explicitOccur,
+      });
+    });
+
+    it("removes dur=null from the data", () => {
+      datumData = {
+        state: { id: "something" },
+        dur: null,
+      };
+      changeDatumCommand(datumData, "start");
+      expect(datumData).toMatchObject({
+        state: { id: "something" },
+      });
+      expect(datumData).not.toHaveProperty("dur");
+    });
+
+    it("adds a dur key to the beginning of the optional keys array", () => {
+      datumData = {
+        state: { id: "something" },
+      };
+      const args: DataArgs = { optional: ["existingKey"] };
+      changeDatumCommand(datumData, "start", args);
+      expect(args.optional).toEqual(["dur", "existingKey"]);
+    });
   });
 
   describe("end", () => {
-    it.todo("adds state.id=false if state doesn't exist");
-    it.todo(
-      "turns a preexisting state into lastState and adds a state.id = false",
-    );
-    it.todo("adds occurTime=now if it doesn't exist");
-    it.todo("removes dur=null from the data");
-    it.todo("adds a dur key to the beginning of the optional keys array");
+    it("adds state.id=false if state doesn't exist", () => {
+      datumData = {
+        existing: "data",
+      };
+      changeDatumCommand(datumData, "end");
+      expect(datumData).toMatchObject({
+        existing: "data",
+        state: {
+          id: false,
+        },
+      });
+    });
+
+    it("turns a preexisting state into lastState and adds a state.id = false", () => {
+      datumData = {
+        state: { id: "outside", weather: "rainy" },
+        field: "environment",
+      };
+      changeDatumCommand(datumData, "end");
+      expect(datumData).toEqual({
+        state: { id: false },
+        field: "environment",
+        lastState: { id: "outside", weather: "rainy" },
+        occurTime,
+      });
+    });
+
+    it("adds occurTime=now if it doesn't exist", () => {
+      datumData = {
+        existing: "data",
+      };
+      changeDatumCommand(datumData, "end");
+      expect(datumData).toMatchObject({
+        occurTime,
+      });
+    });
+
+    it("keeps an existing occurTime", () => {
+      const explicitOccur = toDatumTime("2023-12-21T11:45:00.000");
+      datumData = {
+        occurTime: explicitOccur,
+      };
+      changeDatumCommand(datumData, "end");
+      expect(datumData).toMatchObject({
+        occurTime: explicitOccur,
+      });
+    });
+
+    it("removes dur=null from the data", () => {
+      datumData = {
+        state: { id: "something" },
+        dur: null,
+      };
+      changeDatumCommand(datumData, "end");
+      expect(datumData).toMatchObject({
+        state: { id: "something" },
+      });
+      expect(datumData).not.toHaveProperty("dur");
+    });
+    it("adds a dur key to the beginning of the optional keys array", () => {
+      datumData = {
+        state: { id: "something" },
+      };
+      const args: DataArgs = { optional: ["existingKey"] };
+      changeDatumCommand(datumData, "end", args);
+      expect(args.optional).toEqual(["dur", "existingKey"]);
+    });
   });
 
   describe("occur", () => {
-    it.todo("adds occurTime=now if it doesn't exist");
-    it.todo("adds dur=null to the data");
+    it("adds dur=null to the data", () => {
+      datumData = {
+        state: { id: "something" },
+      };
+      changeDatumCommand(datumData, "occur");
+      expect(datumData).toMatchObject({
+        state: { id: "something" },
+        dur: null,
+      });
+    });
+    it("overwrites an existing dur with null", () => {
+      datumData = {
+        state: { id: "something" },
+        dur: "PT10M",
+      };
+      changeDatumCommand(datumData, "occur");
+      expect(datumData).toMatchObject({
+        state: { id: "something" },
+        dur: null,
+      });
+    });
+
+    it("adds occurTime=now if it doesn't exist", () => {
+      datumData = {
+        existing: "data",
+      };
+      changeDatumCommand(datumData, "occur");
+      expect(datumData).toMatchObject({
+        occurTime,
+      });
+    });
+    it("keeps an existing occurTime", () => {
+      const explicitOccur = toDatumTime("2023-12-21T11:45:00.000");
+      datumData = {
+        occurTime: explicitOccur,
+      };
+      changeDatumCommand(datumData, "occur");
+      expect(datumData).toMatchObject({
+        occurTime: explicitOccur,
+      });
+    });
   });
 
   describe("switch", () => {
-    it.todo("adds occurTime=now if it doesn't exist");
-    it.todo("removes state if it exists");
-    it.todo("removes dur=null from the data");
-    it.todo(
-      "adds a state.id=true and a dur key to the beginning of the optional keys array",
-    );
+    it("sets state to be {}", () => {
+      datumData = {
+        field: "field",
+      };
+      changeDatumCommand(datumData, "switch");
+      expect(datumData).toMatchObject({
+        field: "field",
+        state: {},
+      });
+    });
+
+    it("overwrites existing state with {}", () => {
+      datumData = {
+        state: { id: "something" },
+        occurTime,
+      };
+      changeDatumCommand(datumData, "switch");
+      expect(datumData).toEqual({
+        state: {},
+        occurTime,
+      });
+    });
+
+    it("adds occurTime=now if it doesn't exist", () => {
+      datumData = {
+        existing: "data",
+      };
+      changeDatumCommand(datumData, "switch");
+      expect(datumData).toMatchObject({
+        occurTime,
+      });
+    });
+    it("keeps an existing occurTime", () => {
+      const explicitOccur = toDatumTime("2023-12-21T11:45:00.000");
+      datumData = {
+        occurTime: explicitOccur,
+      };
+      changeDatumCommand(datumData, "switch");
+      expect(datumData).toMatchObject({
+        occurTime: explicitOccur,
+      });
+    });
+
+    it("removes dur=null from the data", () => {
+      datumData = {
+        state: { id: "something" },
+        dur: null,
+      };
+      changeDatumCommand(datumData, "switch");
+      expect(datumData).toEqual({
+        state: {},
+        occurTime,
+      });
+      expect(datumData).not.toHaveProperty("dur");
+    });
+
+    it("keeps an existing dur", () => {
+      const explicitDur = "PT10M";
+      datumData = {
+        state: { id: "something" },
+        dur: explicitDur,
+        occurTime,
+      };
+      changeDatumCommand(datumData, "switch");
+      expect(datumData).toEqual({
+        state: {},
+        dur: explicitDur,
+        occurTime,
+      });
+    });
+
+    it("adds a state.id=true and a dur key to the beginning of the optional keys array", () => {
+      datumData = {
+        state: { id: "something" },
+      };
+      const args: DataArgs = { optional: ["existingKey"] };
+      changeDatumCommand(datumData, "switch", args);
+      expect(args.optional).toEqual(["state.id=true", "dur", "existingKey"]);
+    });
   });
 });
