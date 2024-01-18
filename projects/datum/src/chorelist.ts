40211c31c716ea904c43b137a985f4c65b6203dc
Nate Faber <nathaniel@njfaber.com>
2024-01-18 16:37:09 +0100

    first pass at new chorelist

--- projects/datum/src/chorelist.ts
+++ projects/datum/src/chorelist.ts
@@ -3,6 +3,8 @@ import { choreView } from "../views";
 import { Show } from "../../../src/input/outputArgs";
 import { DateTime } from "luxon";
 import chalk from "chalk";
+import { ReduceRow } from "../../../src/views/DatumView";
+import Table from "easy-table";
 
 async function chorelist() {
   const choresResponse = await reduceCmd({
@@ -10,31 +12,56 @@ async function chorelist() {
     groupLevel: 1,
     show: Show.None,
   });
-  const chores = choresResponse.rows
+  const choreRows = choresResponse.rows as ReduceRow<typeof choreView>[];
+  const chores = choreRows
     .filter((chore) => chore.value.next !== undefined)
-    .sort((a, b) => a.value.next - b.value.next);
+    .sort((a, b) => a.value.iti ?? 9e99 - (b.value.iti ?? 9e99));
   const now = DateTime.local().toISODate();
-  const due = chores.filter((row) => row.value.next <= now);
-  const future = chores.filter((row) => row.value.next > now);
-  const tenDash = "----------";
-  const header = "Last Done \tNext Date \tChore";
-  console.log(header);
-  if (due.length === 0) {
-    if (process.stdout.isTTY) {
-      console.log(chalk.green("COMPLETED!\tGOOD WORK!\t\\(◦'⌣'◦)/"));
+
+  const t = new Table();
+
+  let totalDue = 0;
+  chores.forEach((row) => {
+    if (row.value.next === undefined) {
+      return;
     }
-  } else {
-    console.log(
-      chalk.red(due.map((row) => choreRowToLastNextChore(row)).join("\n")),
+    const next = row.value.next.slice(0, 10);
+    const last =
+      row.value.lastOccur === "0000-00-00"
+        ? "NEVER"
+        : DateTime.fromISO(row.value.lastOccur).toISODate();
+    const due = next <= now;
+    const doneToday = last === now;
+    if (!due) {
+      totalDue += 1;
+    }
+    if (!due && !doneToday) {
+      return;
+    }
+    const iti =
+      row.value.iti === undefined ? undefined : Math.floor(row.value.iti);
+    const daysOverdue = Math.floor(
+      DateTime.fromISO(now).diff(DateTime.fromISO(next), "days").days,
     );
-    if (process.stdout.isTTY) {
-      console.log(tenDash + "\t" + tenDash + "\t" + tenDash);
-    }
+    const color =
+      iti === undefined
+        ? chalk.white
+        : daysOverdue > iti
+          ? chalk.red
+          : daysOverdue > iti / 2
+            ? chalk.yellow
+            : chalk.white;
+
+    t.cell("Chore", color(row.key));
+    t.cell("ITI", color(iti));
+    t.cell("Due", color(next));
+    t.cell("Last Done", color(last));
+    t.newRow();
+  });
+  console.log(t.toString());
+  if (totalDue === 0) {
+    console.log(chalk.green("COMPLETED!\tGOOD WORK!\t\\(◦'⌣'◦)/"));
   }
-  const futureColor = due.length === 0 ? chalk.green : chalk.white;
-  console.log(
-    futureColor(future.map((row) => choreRowToLastNextChore(row)).join("\n")),
-  );
 }
 
 if (require.main === module) {
