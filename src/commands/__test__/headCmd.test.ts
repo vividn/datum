cd212b9eedc16fbc64c53d0f5387aebd43092a46
Nate Faber <nathaniel@njfaber.com>
2024-02-26 15:08:35 +0100

    Lint code

--- src/commands/__test__/headCmd.test.ts
+++ src/commands/__test__/headCmd.test.ts
@@ -212,8 +212,8 @@ describe("headCmd", () => {
     );
     expect(utcDates.every((date) => date === today)).toBe(false);
 
-    const localDates = docs.map(
-      (doc) => datumTimeToLuxon(doc.data.occurTime)?.toISODate(),
+    const localDates = docs.map((doc) =>
+      datumTimeToLuxon(doc.data.occurTime)?.toISODate(),
     );
     expect(localDates.every((date) => date === today)).toBe(true);
 
