d1be7f7a97d22dceb27ebf26a0324857c04fb063
Nate Faber <nathaniel@njfaber.com>
2024-03-11 15:20:56 +0100

    write some test titles

---
+++ src/output/__test__/tableOutput.test.ts
@@ -0,0 +1,8 @@
+describe("tableOutput", () => {
+  it.todo("Returns a table for an array of documents");
+  it.todo("Can return a table with a different time metric");
+  it.todo("Returns undefined if show is None");
+  it.todo("Returns just the headers if documents is an empty array");
+  it.todo("Can return with extra columns");
+  it.todo("Can return using a format string");
+});
