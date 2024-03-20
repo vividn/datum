593b43628a23c849e61c608cd34c9c1be34b43c7
Nate Faber <nathaniel@njfaber.com>
2024-03-20 11:55:31 +0100

    Chores fix null next date (#695)

--- src/views/insertDatumView.ts
+++ src/views/insertDatumView.ts
@@ -4,24 +4,29 @@ import {
   StringifiedDatumView,
   ViewDocument,
 } from "./DatumView";
-import { addDoc } from "../documentControl/addDoc";
+import { addDoc, ConflictStrategyNames } from "../documentControl/addDoc";
 import { BaseDocControlArgs } from "../documentControl/base";
 
 type InsertDatumViewArgs = {
   datumView: DatumView<any, any, any, any> | StringifiedDatumView;
+  conflictStrategy?: Extract<
+    ConflictStrategyNames,
+    "overwrite" | "useOld" | "fail"
+  >;
 } & BaseDocControlArgs;
 
 export async function insertDatumView({
   db,
   datumView,
   outputArgs,
+  conflictStrategy = "overwrite",
 }: InsertDatumViewArgs): Promise<ViewDocument> {
   const viewPayload = datumViewToViewPayload(datumView);
   const newDesignDoc = (await addDoc({
     db,
     payload: viewPayload,
     outputArgs: outputArgs,
-    conflictStrategy: "overwrite",
+    conflictStrategy,
   })) as ViewDocument;
   return newDesignDoc;
 }
