b708d27941eabdd41909636cb55e795920b90864
Nate Faber <nathaniel@njfaber.com>
2024-07-29 17:08:35 +0200

    start making types for migraitons

--- src/migrations/migrations.ts
+++ src/migrations/migrations.ts
@@ -1,6 +1,15 @@
+import { DatumData, DatumMetadata, EitherDocument } from "../documentControl/DatumDocument";
+
 export function getMigrationViewName(shortName: string): string {
   return `migrate__${shortName}`;
 }
 export function getMigrationId(shortName: string): string {
   return `_design/${getMigrationViewName(shortName)}`;
+}
+
+export function migrationEmit(priority: number, { op: }) {}
+
+export type DatumMigration = {
+  name: string;
+  map: (doc: EitherDocument) => void;
 }
