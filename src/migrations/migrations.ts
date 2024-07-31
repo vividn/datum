4e0a5c121f78a4b17f2b8151013aa404de6f2303
Nate Faber <nathaniel@njfaber.com>
2024-07-31 15:11:53 +0200

    keep working on types

--- src/migrations/migrations.ts
+++ src/migrations/migrations.ts
@@ -1,4 +1,8 @@
-import { DatumData, DatumMetadata, EitherDocument } from "../documentControl/DatumDocument";
+import { UpdateStrategyNames } from "../documentControl/combineData";
+import { EitherDocument } from "../documentControl/DatumDocument";
+import { GenericObject } from "../GenericObject";
+import { DatumView } from "../views/DatumView";
+import { _emit } from "../views/emit";
 
 export function getMigrationViewName(shortName: string): string {
   return `migrate__${shortName}`;
@@ -7,9 +11,46 @@ export function getMigrationId(shortName: string): string {
   return `_design/${getMigrationViewName(shortName)}`;
 }
 
-export function migrationEmit(priority: number, { op: }) {}
+export type MigrationOps = UpdateStrategyNames | "overwrite" | "delete";
+const MigrationOps: Record<MigrationOps, any> = {
+  merge: "",
+  useOld: "",
+  preferOld: "",
+  update: "",
+  preferNew: "",
+  useNew: "",
+  removeConflicting: "",
+  xor: "",
+  intersection: "",
+  append: "",
+  prepend: "",
+  appendSort: "",
+  mergeSort: "",
+  overwrite: "",
+  delete: "",
+};
+export const migrationOps = Object.keys(MigrationOps);
 
-export type DatumMigration = {
-  name: string;
-  map: (doc: EitherDocument) => void;
+type MigrationMapKey = any; // user defined sort for orderin migration operations
+type MigrationMapValue = {
+  op: MigrationOps;
+  value: GenericObject;
+};
+
+export function migrationEmit(
+  key: MigrationMapValue,
+  value: MigrationMapValue,
+) {
+  _emit(key, value);
 }
+
+export type DatumMigration = DatumView<
+  EitherDocument,
+  MigrationMapKey,
+  MigrationMapValue,
+  undefined
+> & {
+  name: `migrate_${string}`;
+  reduce: undefined;
+  namedReduce: undefined;
+};
