d881968c496373e7dcdd6cf0d12657df8abd28b6
Nate Faber <nathaniel@njfaber.com>
2024-07-31 17:28:34 +0200

    get rekey tested and working

--- src/utils/__test__/rekey.test.ts
+++ src/utils/__test__/rekey.test.ts
@@ -1,4 +1,4 @@
-import { rekey } from "../rekey";
+import { rekey, RekeyError } from "../rekey";
 
 describe("rekey", () => {
   it("renames keys in aData based off of the key value pairs in bData", () => {
@@ -26,10 +26,46 @@ describe("rekey", () => {
   });
 
   it("can rename nested keys", () => {
-    const aData = { a: { b: 1, c: 2 }, d: 3 };
-    const bData = { a: { b: "B" } };
-    const expected = { a: { B: 1, c: 2 }, d: 3 };
+    const aData = { a: { b: 1, c: 2, e: 5 }, d: 3 };
+    const bData = { a: { b: "B", c: "C" } };
+    const expected = { a: { B: 1, C: 2, e: 5 }, d: 3 };
     const actual = rekey(aData, bData);
     expect(actual).toEqual(expected);
+  });
+
+  it("throws an error if bData has any values that aren't strings", () => {
+    const aData = { a: 1, b: 2, c: 3 };
+    const bData = { a: "A", b: 2 };
+    expect(() => rekey(aData, bData)).toThrow(RekeyError);
+
+    const bData2 = { a: "A", b: null };
+    expect(() => rekey(aData, bData2)).toThrow(RekeyError);
+
+    const bData3 = { a: "A", b: undefined };
+    expect(() => rekey(aData, bData3)).toThrow(RekeyError);
+
+    const bData4 = { a: "A", b: 3 };
+    expect(() => rekey(aData, bData4)).toThrow(RekeyError);
+
+    const bData5 = { a: "A", b: ["a", "b"] };
+    expect(() => rekey(aData, bData5)).toThrow(RekeyError);
+  });
+
+  it("throws an error for a nested rekey if a isn't also nested", () => {
+    const aData = { a: 1, b: 2, c: 3 };
+    const bData = { a: { b: "B" } };
+    expect(() => rekey(aData, bData)).toThrow(RekeyError);
+  });
+
+  it("should clobber existing keys", () => {
+    const aData = { a: 1, b: 2, c: 3 };
+    const bData = { a: "b" };
+    const expected = { b: 1, c: 3 };
+    const actual = rekey(aData, bData);
+    expect(actual).toEqual(expected);
+
+    const bData2 = { b: "a" };
+    const expected2 = { a: 2, c: 3 };
+    expect(rekey(aData, bData2)).toEqual(expected2);
   });
 });
--- src/utils/rekey.ts
+++ src/utils/rekey.ts
@@ -1,7 +1,37 @@
+import isPlainObject from "lodash.isplainobject";
 import { GenericObject } from "../GenericObject";
+import { MyError } from "../errors";
 
-export function rekey(aData: GenericObject, bData: GenericObject): GenericObject {
-  
-  const newData = { ...aData, ...bData };
+export class RekeyError extends MyError {
+  constructor(m: unknown) {
+    super(m);
+    Object.setPrototypeOf(this, RekeyError.prototype);
+  }
+}
+
+export function rekey(
+  aData: GenericObject,
+  bData: GenericObject,
+): GenericObject {
+  const newData: GenericObject = {};
+
+  for (const key in aData) {
+    if (!Object.keys(bData).includes(key)) {
+      // Renaming should clobber existing keys, so if newData already has it don't overwrite
+      newData[key] ??= aData[key];
+      continue;
+    }
+    if (isPlainObject(aData[key]) && isPlainObject(bData[key])) {
+      newData[key] = rekey(aData[key], bData[key]);
+      continue;
+    }
+
+    if (typeof bData[key] !== "string") {
+      throw new RekeyError("rekey values must be strings");
+    }
+
+    newData[bData[key]] = aData[key];
+  }
+
   return newData;
 }
