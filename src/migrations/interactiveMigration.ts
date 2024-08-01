6b8b938627303072a86031585556a8316518c7c3
Nate Faber <nathaniel@njfaber.com>
2024-08-01 18:13:22 +0200

    make first draft of interactive migration

---
+++ src/migrations/interactiveMigration.ts
@@ -0,0 +1,60 @@
+import readline from "node:readline/promises";
+import { MigrationMapRow, migrationName } from "./migrations";
+import { getCmd } from "../commands/getCmd";
+import stringify from "string.ify";
+import chalk from "chalk";
+import { jClone } from "../utils/jClone";
+import { OutputArgs, Show } from "../input/outputArgs";
+import { migrateOne } from "./migrateOne";
+
+export async function interactiveMigration({
+  name,
+  db,
+  outputArgs: originalOutputArgs = {},
+}: {
+  name: string;
+  db: PouchDB.Database;
+  outputArgs: OutputArgs;
+}): Promise<void> {
+  const outputArgs = jClone(originalOutputArgs);
+  const showToggle =
+    outputArgs.show === Show.All ? Show.Standard : outputArgs.show;
+  const migrationRows = (await db.query(migrationName(name), { reduce: false }))
+    .rows as MigrationMapRow[];
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  while (migrationRows.length > 0) {
+    const row = migrationRows[0];
+    const {
+      value: { op, data },
+    } = row;
+    console.info("----------------------");
+    await getCmd(row.id, outputArgs);
+    console.info(chalk.yellow(`~~~~ OP: ${op} ~~~~`));
+    console.info(stringify(data));
+    console.info("----------------------");
+
+    const answer = await rl.question(
+      "Migrate? [y]es, [n]o, [q]uit, toggle [s]how all: ",
+    );
+    if (answer === "q") {
+      break;
+    } else if (answer === "s") {
+      outputArgs.show = outputArgs.show === Show.All ? showToggle : Show.All;
+    } else if (answer === "y") {
+      await migrateOne({ row, db, outputArgs: outputArgs });
+      migrationRows.shift();
+    } else if (answer === "n") {
+      migrationRows.shift();
+    } else {
+      console.error("Invalid input");
+    }
+  }
+
+  // Your migration code here
+  console.log("Migration completed");
+  rl.close();
+}
