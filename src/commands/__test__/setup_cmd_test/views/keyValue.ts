c84b8304a512fe6474ccd64279ad2ff58398a1f9
Nate Faber <nathaniel@njfaber.com>
2024-08-29 13:54:31 +0200

    more fixes

--- src/commands/__test__/setup_cmd_test/views/keyValue.ts
+++ src/commands/__test__/setup_cmd_test/views/keyValue.ts
@@ -1,13 +1,13 @@
 import { _emit } from "../../../../views/emit";
 import { DatumView } from "../../../../views/DatumView";
+import { DatumDocument } from "../../../../documentControl/DatumDocument";
 
 function emit(key: unknown, value: unknown): void {
   _emit(key, value);
 }
 
-export const keyValueView: DatumView<any, any, any, undefined> = {
+export const keyValueView: DatumView<DatumDocument> = {
   name: "key_value_view",
-  emit,
   map: (doc) => {
     if (doc.data.key && doc.data.value) {
       emit(doc.data.key, doc.data.value);
