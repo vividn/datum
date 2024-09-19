fdc60c812c728dbc64eb4ed190dcda8263e8dc04
Nate Faber <nathaniel@njfaber.com>
2024-09-19 14:48:09 +0200

    add test placeholder

--- src/commands/__test__/initCmd.test.ts
+++ src/commands/__test__/initCmd.test.ts
@@ -3,4 +3,5 @@ describe("initCmd", () => {
   it("writes datumrc into the xdg_config directory");
   it("fails if datumrc exists and --overwrite is not specified");
   it("overwrites datumrc if it exists and --overwrite is specified");
+  it("creates the project directory, and populates it with some files");
 });
