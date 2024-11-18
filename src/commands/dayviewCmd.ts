21d6f8e4e254e025a26bc7358d33ffbdb7824d4e
Nate Faber <nathaniel@njfaber.com>
2024-11-18 17:31:21 +0100

    add some argparse options

--- src/commands/dayviewCmd.ts
+++ src/commands/dayviewCmd.ts
@@ -9,6 +9,40 @@ export const dayviewArgs = new ArgumentParser({
   parents: [],
 });
 
+dayviewArgs.add_argument("--n-days", {
+  help: "number of days to show",
+  type: "int",
+  dest: "nDays",
+});
+
+dayviewArgs.add_argument("--start-date", {
+  help: "start date to show",
+  type: "str",
+  dest: "startDate",
+});
+
+dayviewArgs.add_argument("--end-date", {
+  help: "end date to show. Defaults to today",
+  type: "str",
+  dest: "endDate",
+});
+
+dayviewArgs.add_argument("--width", {
+  help: "width of the svg",
+  type: "int",
+});
+
+dayviewArgs.add_argument("--height", {
+  help: "height of the svg",
+  type: "int",
+});
+
+dayviewArgs.add_argument("--day-height", {
+  help: "height of each day. If not specified, it will be calculated",
+  type: "int",
+  dest: "dayHeight",
+});
+
 export const dayviewCmdArgs = new ArgumentParser({
   description: "View the day",
   prog: "datum dayview",
@@ -16,7 +50,14 @@ export const dayviewCmdArgs = new ArgumentParser({
   parents: [dayviewArgs, dbArgs],
 });
 
-export type DayviewCmdArgs = MainDatumArgs;
+export type DayviewCmdArgs = MainDatumArgs & {
+  nDays?: number;
+  startDate?: string;
+  endDate?: string;
+  width?: number;
+  height?: number;
+  dayHeight?: number;
+};
 
 export async function dayviewCmd(
   args: DayviewCmdArgs | string | string[],
