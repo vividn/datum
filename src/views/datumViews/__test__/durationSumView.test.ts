794771b2778bac03a1ae8b8aa1139502a436dd1e
Nate Faber <nathaniel@njfaber.com>
2025-01-29 15:45:38 +0100

    Lint code

--- src/views/datumViews/__test__/durationSumView.test.ts
+++ src/views/datumViews/__test__/durationSumView.test.ts
@@ -1,3 +1,6 @@
 describe("durationSumView", () => {
-it.todo("can be summed to get the total number of seconds for each state");
-it.todo("emits negative values for the current state, and positive values for the last state")
+  it.todo("can be summed to get the total number of seconds for each state");
+  it.todo(
+    "emits negative values for the current state, and positive values for the last state",
+  );
+});
