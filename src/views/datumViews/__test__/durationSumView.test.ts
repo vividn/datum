6ece9be5a54588a2e6eb898ece8df97f6d3d6b1b
Nate Faber <nathaniel@njfaber.com>
2025-01-29 15:31:46 +0100

    add a duration sum view to calculate seconds of various states

---
+++ src/views/datumViews/__test__/durationSumView.test.ts
@@ -0,0 +1,3 @@
+describe("durationSumView", () => {
+it.todo("can be summed to get the total number of seconds for each state");
+it.todo("emits negative values for the current state, and positive values for the last state")
