5871953feedd1d9a3af36efcb6c2b20c349b6527
Justin Shenk <shenk.justin@gmail.com>
2025-02-18 14:31:33 +0100

    add react frontend and backend api

---
+++ projects/datum-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DatumDashboard from './DatumDashboard';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DatumDashboard />
+    </div>
+  )
+}
+
+export default App;
