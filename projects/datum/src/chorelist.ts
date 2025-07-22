839c3f89d53eb0c75a5cae99efe2a920034c82aa
Nate Faber <nathaniel@njfaber.com>
2025-07-22 15:35:15 +0200

    Change default db to vnhome for chorelist (#936)

--- projects/datum/src/chorelist.ts
+++ projects/datum/src/chorelist.ts
@@ -238,6 +238,7 @@ chorelistArgs.add_argument("--update", {
 
 async function chorelistCmd(argsOrCli: ChorelistArgs | string | string[]) {
   const args = parseIfNeeded(chorelistArgs, argsOrCli);
+  args.db ??= "vnhome";
   if (args.watch) {
     await chorelistwatch(args);
   } else {
